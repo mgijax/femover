89a603e23138926ab72feb6a3a45fc05194b57cc
Jonathan Beal <Jon.Beal@jax.org>
2010-08-02 10:52:03 +0000

    revised to handle errors in bulk load process with non-zero exit code

--- control/bulkLoadPostgres.sh
+++ control/bulkLoadPostgres.sh
@@ -6,9 +6,12 @@
 PGPASSWORD=$4
 export PGPASSWORD
 
-psql -h $1 -d $2 -U $3 <<END
-\copy $6 from '$5' with null as ''
-\g
-vacuum analyze $6;
-END
+psql -h $1 -d $2 -U $3 --command "\copy $6 from '$5' with null as ''" > ../logs/$6.load.log 2>&1
+EXITCODE=$?
+
+if [ ${EXITCODE} -ne 0 ]; then
+	exit ${EXITCODE}
+fi
+
+psql -h $1 -d $2 -U $3 --command "vacuum analyze $6;" > ../logs/$6.vacuum.log 2>&1
 exit $?
