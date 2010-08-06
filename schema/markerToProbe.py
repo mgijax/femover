ad4b530b3e126e160308a718ad67b161479a6e2d
Jonathan Beal <Jon.Beal@jax.org>
2010-08-06 17:58:17 +0000

    initial addition

---
+++ schema/markerToProbe.py
@@ -0,0 +1,38 @@
+#!/usr/local/bin/python
+
+import Table
+
+# contains data definition information for the markerToProbe table
+
+###--- Globals ---###
+
+# name of this database table
+tableName = 'markerToProbe'
+
+# MySQL statement to create this table
+createStatement = '''CREATE TABLE %s  ( 
+	uniqueKey	int		NOT NULL,
+	markerKey	int 		NOT NULL,
+	probeKey	int		NOT NULL,
+	referenceKey	int		NOT NULL,
+	qualifier	varchar(80)	NULL,
+	PRIMARY KEY(uniqueKey))''' % tableName
+
+# Maps from index suffix to create statement for that index.  In each
+# statement, the first %s is for the index name, and the second is for the
+# table name.
+indexes = {
+	'probeKey' : 'create index %s on %s (probeKey, markerKey)',
+	'markerKey' : 'create index %s on %s (markerKey, probeKey)',
+	'referenceKey' : 'create index %s on %s (referenceKey)',
+	}
+
+# global instance of this Table object
+table = Table.Table (tableName, createStatement, indexes)
+
+###--- Main program ---###
+
+# if executed as a script, pass the global Table object into the general
+# main program for Table subclasses
+if __name__ == '__main__':
+	Table.main(table)
---
+++ schema/probeToSequence.py
@@ -0,0 +1,38 @@
+#!/usr/local/bin/python
+
+import Table
+
+# contains data definition information for the probeToSequence table
+
+###--- Globals ---###
+
+# name of this database table
+tableName = 'probeToSequence'
+
+# MySQL statement to create this table
+createStatement = '''CREATE TABLE %s  ( 
+	uniqueKey	int		NOT NULL,
+	probeKey	int		NOT NULL,
+	sequenceKey	int 		NOT NULL,
+	referenceKey	int		NOT NULL,
+	qualifier	varchar(80)	NULL,
+	PRIMARY KEY(uniqueKey))''' % tableName
+
+# Maps from index suffix to create statement for that index.  In each
+# statement, the first %s is for the index name, and the second is for the
+# table name.
+indexes = {
+	'probeKey' : 'create index %s on %s (probeKey, sequenceKey)',
+	'sequenceKey' : 'create index %s on %s (sequenceKey, probeKey)',
+	'referenceKey' : 'create index %s on %s (referenceKey)',
+	}
+
+# global instance of this Table object
+table = Table.Table (tableName, createStatement, indexes)
+
+###--- Main program ---###
+
+# if executed as a script, pass the global Table object into the general
+# main program for Table subclasses
+if __name__ == '__main__':
+	Table.main(table)
