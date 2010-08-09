bcd6ac1f5edb0e2de4b4203fac6d213426842557
Jonathan Beal <Jon.Beal@jax.org>
2010-08-09 16:57:34 +0000

    added; renamed from markerOrthology.py

---
+++ schema/marker_orthology.py
@@ -0,0 +1,38 @@
+#!/usr/local/bin/python
+
+import Table
+
+# contains data definition information for the marker_orthology table
+
+###--- Globals ---###
+
+# name of this database table
+tableName = 'marker_orthology'
+
+# MySQL statement to create this table
+createStatement = '''CREATE TABLE %s  ( 
+	unique_key		int	NOT NULL,
+	mouse_marker_key	int	NOT NULL,
+	other_marker_key	int	NOT NULL,
+	other_symbol		varchar(50)	NULL,
+	other_organism		varchar(50)	NULL,
+	PRIMARY KEY(unique_key))''' % tableName
+
+# Maps from index suffix to create statement for that index.  In each
+# statement, the first %s is for the index name, and the second is for the
+# table name.
+indexes = {
+	'mouse_marker' : 'create index %s on %s (mouse_marker_key)',
+	'other_marker' : 'create index %s on %s (other_marker_key)',
+	'other_organism' : 'create index %s on %s (other_organism)',
+	}
+
+# global instance of this Table object
+table = Table.Table (tableName, createStatement, indexes)
+
+###--- Main program ---###
+
+# if executed as a script, pass the global Table object into the general
+# main program for Table subclasses
+if __name__ == '__main__':
+	Table.main(table)
