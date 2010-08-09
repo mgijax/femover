ff54da17c2f54d36543776643c72f0c73549040e
Jonathan Beal <Jon.Beal@jax.org>
2010-08-09 16:57:39 +0000

    added; renamed from markerToReference.py

---
+++ schema/marker_to_reference.py
@@ -0,0 +1,36 @@
+#!/usr/local/bin/python
+
+import Table
+
+# contains data definition information for the marker_to_reference table
+
+###--- Globals ---###
+
+# name of this database table
+tableName = 'marker_to_reference'
+
+# MySQL statement to create this table
+createStatement = '''CREATE TABLE %s  ( 
+	unique_key	int	NOT NULL,
+	marker_key	int	NOT NULL,
+	reference_key	int	NOT NULL,
+	qualifier	varchar(80)	NULL,
+	PRIMARY KEY(unique_key))''' % tableName
+
+# Maps from index suffix to create statement for that index.  In each
+# statement, the first %s is for the index name, and the second is for the
+# table name.
+indexes = {
+	'marker_key' : 'create index %s on %s (marker_key, reference_key)',
+	'reference_key' : 'create index %s on %s (reference_key, marker_key)',
+	}
+
+# global instance of this Table object
+table = Table.Table (tableName, createStatement, indexes)
+
+###--- Main program ---###
+
+# if executed as a script, pass the global Table object into the general
+# main program for Table subclasses
+if __name__ == '__main__':
+	Table.main(table)
