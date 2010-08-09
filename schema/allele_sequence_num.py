5d8c1cfa52e82f02bea968abcbda708caa11064e
Jonathan Beal <Jon.Beal@jax.org>
2010-08-09 16:58:11 +0000

    added; renamed from alleleSequenceNum.py

---
+++ schema/allele_sequence_num.py
@@ -0,0 +1,33 @@
+#!/usr/local/bin/python
+
+import Table
+
+# contains data definition information for the alleleSequenceNum table
+
+###--- Globals ---###
+
+# name of this database table
+tableName = 'allele_sequence_num'
+
+# MySQL statement to create this table
+createStatement = '''CREATE TABLE %s  ( 
+	allele_key	int		not null,
+	by_symbol	int		not null,
+	by_allele_type	int		not null,
+	by_primary_id	int		not null,
+	PRIMARY KEY(allele_key))''' % tableName
+
+# Maps from index suffix to create statement for that index.  In each
+# statement, the first %s is for the index name, and the second is for the
+# table name.
+indexes = {}
+
+# global instance of this Table object
+table = Table.Table (tableName, createStatement, indexes)
+
+###--- Main program ---###
+
+# if executed as a script, pass the global Table object into the general
+# main program for Table subclasses
+if __name__ == '__main__':
+	Table.main(table)
