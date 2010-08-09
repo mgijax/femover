758de161673067006dd09068e2969821f5a8c51f
Jonathan Beal <Jon.Beal@jax.org>
2010-08-09 16:56:17 +0000

    added; renamed from referenceAbstractGatherer.py

---
+++ gather/reference_abstract_gatherer.py
@@ -0,0 +1,35 @@
+#!/usr/local/bin/python
+# 
+# gathers data for the 'referenceAbstract' table in the front-end database
+
+import Gatherer
+
+###--- Classes ---###
+
+ReferenceAbstractGatherer = Gatherer.Gatherer
+	# Is: a data gatherer for the referenceAbstract table
+	# Has: queries to execute against Sybase
+	# Does: queries Sybase for primary data for reference abstracts,
+	#	collates results, writes tab-delimited text file
+
+###--- globals ---###
+
+cmds = [
+	'select _Refs_key as referenceKey, abstract from bib_refs',
+	]
+
+# order of fields (from the query results) to be written to the output file
+fieldOrder = [ 'referenceKey', 'abstract', ]
+
+# prefix for the filename of the output file
+filenamePrefix = 'referenceAbstract'
+
+# global instance of a ReferenceAbstractGatherer
+gatherer = ReferenceAbstractGatherer (filenamePrefix, fieldOrder, cmds)
+
+###--- main program ---###
+
+# if invoked as a script, use the standard main() program for gatherers and
+# pass in our particular gatherer
+if __name__ == '__main__':
+	Gatherer.main (gatherer)
