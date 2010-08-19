82ab4694668754e15b23d68d8fb99ffb0a6616c5
Jonathan Beal <Jon.Beal@jax.org>
2010-08-19 10:37:12 +0000

    initial addition

---
+++ gather/allele_synonym_gatherer.py
@@ -0,0 +1,40 @@
+#!/usr/local/bin/python
+# 
+# gathers data for the 'allele_synonym' table in the front-end database
+
+import Gatherer
+
+###--- Classes ---###
+
+AlleleSynonymGatherer = Gatherer.Gatherer
+	# Is: a data gatherer for the allele_synonym table
+	# Has: queries to execute against the source database
+	# Does: queries the source database for synonyms for alleles,
+	#	collates results, writes tab-delimited text file
+
+###--- globals ---###
+
+cmds = [
+	'''select s._Object_key as _Allele_key, s.synonym, t.synonymType
+		from mgi_synonym s, mgi_synonymtype t
+		where s._SynonymType_key = t._SynonymType_key
+			and t._MGIType_key = 11
+		order by s._Object_key, s.synonym''',
+	]
+
+# order of fields (from the query results) to be written to the
+# output file
+fieldOrder = [ Gatherer.AUTO, '_Allele_key', 'synonym', 'synonymType' ]
+
+# prefix for the filename of the output file
+filenamePrefix = 'allele_synonym'
+
+# global instance of a AlleleSynonymGatherer
+gatherer = AlleleSynonymGatherer (filenamePrefix, fieldOrder, cmds)
+
+###--- main program ---###
+
+# if invoked as a script, use the standard main() program for gatherers and
+# pass in our particular gatherer
+if __name__ == '__main__':
+	Gatherer.main (gatherer)
---
+++ gather/recombinase_gatherer.py
@@ -0,0 +1,542 @@
+#!/usr/local/bin/python
+# 
+# gathers data for the recombinase tables in the front-end database.
+#
+# Because we are generating unique keys that only exist in the front-end
+# database, and because we need those keys to drive relationships among the
+# recombinase tables, we generate all those connected tables within this
+# single script.  (This allows us to keep the keys in sync.)
+
+import Gatherer
+import logger
+import symbolsort
+
+###--- Functions ---###
+
+def explode (x):
+	# expand list x into a new list such that for each element i of x, we
+	# have a tuple (i, all elements of x other than i)
+
+	out = []
+	for i in x:
+		no_i = []
+		for j in x:
+			if i == j:
+				continue
+			no_i.append (j)
+		out.append ( (i, no_i) )
+	return out
+
+###--- Classes ---###
+
+class RecombinaseGatherer (Gatherer.MultiFileGatherer):
+	# Is: a data gatherer for the recombinase tables
+	# Has: queries to execute against the source database
+	# Does: queries Sybase for primary data for recombinases,
+	#	collates results, writes multiple tab-delimited text files
+
+	def findAlleleSystemPairs (self):
+		# Purpose: processes query 0 to identify allele/system pairs
+		# Returns: alleleSystemMap, alleleData, columns, rows
+
+		# alleleSystemMap[allele key] = {system : all/sys key}
+		alleleSystemMap = {}
+
+		# query 0 - defines allele / system pairs.  We need to number
+		# these and store them in data set 0
+
+		cols, rows = self.results[0]
+		keyCol = Gatherer.columnNumber (cols, '_Allele_key')
+		idCol = Gatherer.columnNumber (cols, 'accID')
+		sysCol = Gatherer.columnNumber (cols, 'system')
+		symCol = Gatherer.columnNumber (cols, 'symbol')
+
+		alleleData = {}		# alleleData[key] = (ID, symbol)
+
+		lastKey = None
+		out = []
+		i = 0
+		for row in rows:
+
+			# make sure to add the system for the allele key
+
+			key = row[keyCol]
+			system = row[sysCol]
+			i = i + 1
+
+			if alleleSystemMap.has_key(key):
+				alleleSystemMap[key][system] = i
+			else:
+				alleleSystemMap[key] = { system : i }
+
+			out.append ( (i, key, row[idCol], row[sysCol]) )
+
+			# if this is not the same as the last allele, then
+			# we need to cache its ID and symbol
+
+			if key == lastKey:
+				continue
+			alleleData[key] = (row[idCol], row[symCol])
+
+			lastKey = key
+
+		columns = [ 'alleleSystemKey', 'alleleKey', 'alleleID',
+			'system' ]
+
+		logger.debug ('Found %d allele/system pairs' % i)
+		logger.debug ('Found %d alleles' % len(alleleData))
+
+		return alleleSystemMap, alleleData, columns, out
+
+	def findOtherSystems (self, alleleSystemMap):
+		# Purpose: processes 'alleleSystemMap' to find other systems
+		#	involved with the allele for each allele/system pair
+		#	(does not touch query results in self.results)
+		# Returns: columns, rows
+
+		# we need to generate the data set for
+		# the recombinase_other_system table (for each allele/system
+		# pair, list the other systems associated with each allele)
+
+		out = []
+		i = 0
+		alleles = alleleSystemMap.keys()
+		for allele in alleles:
+			systems = alleleSystemMap[allele].keys()
+			systems.sort()
+
+			for (system, otherSystems) in explode(systems):
+				alleleSystemKey = \
+					alleleSystemMap[allele][system]
+				for otherSystem in otherSystems:
+					i = i + 1
+					out.append ( (i, alleleSystemKey,
+						otherSystem) )
+
+		columns = [ 'uniqueKey', 'alleleSystemKey', 'system' ]
+
+		logger.debug ('Found %d other systems' % i)
+
+		return columns, out
+
+	def findOtherAlleles (self, alleleSystemMap, alleleData):
+		# Purpose: processes 'alleleSystemMap' to find other alleles
+		#	involved with the system for each allele/system pair
+		#	(does not touch query results in self.results)
+		# Returns: columns, rows
+
+		# we will generate the data set for the
+		# recombinase_other_allele table (for each allele/system pair,
+		# list the other alleles associated with each system)
+
+		bySystem = {}		# bySystem[system] = list of alleles
+
+		alleles = alleleSystemMap.keys()
+		for allele in alleles:
+			systems = alleleSystemMap[allele].keys()
+			for system in systems:
+				if not bySystem.has_key (system):
+					bySystem[system] = [ allele ]
+				else:
+					bySystem[system].append (allele)
+
+		out = []
+		i = 0
+
+		for system in bySystem.keys():
+			alleles = bySystem[system]
+			for (allele, otherAlleles) in explode(alleles):
+				alleleSystemKey = \
+					alleleSystemMap[allele][system]
+
+				for other in otherAlleles:	
+					i = i + 1
+					out.append ( (
+						i,
+						alleleSystemKey,
+						other,
+						alleleData[other][0],
+						alleleData[other][1],
+						) )
+
+		columns = [ 'uniqueKey', 'alleleSystemKey', 'alleleKey',
+				'alleleID', 'alleleSymbol', ]
+
+		logger.debug ('Found %d other alleles' % i)
+
+		return columns, out
+
+	def findGenotypes (self):
+		# extract allelic composition and background strain from
+		# query 1
+
+		keyCol = Gatherer.columnNumber (self.results[1][0],
+			'_Genotype_key')
+		noteCol = Gatherer.columnNumber (self.results[1][0], 'note')
+		strainCol = Gatherer.columnNumber (self.results[1][0],
+			'strain')
+
+		alleleComp = {}
+		strain = {}
+
+		for r in self.results[1][1]:
+			key = r[keyCol]
+			if alleleComp.has_key (key):
+				alleleComp[key] = alleleComp[key] + r[noteCol]
+			else:
+				alleleComp[key] = r[noteCol]
+				strain[key] = r[strainCol]
+
+		logger.debug ('Found %d genotypes' % len(strain))
+		return alleleComp, strain
+
+	def findAssayNotes (self):
+		# extract assay note from query 2
+
+		keyCol = Gatherer.columnNumber (self.results[2][0],
+			'_Assay_key')
+		noteCol = Gatherer.columnNumber (self.results[2][0],
+			'assayNote')
+
+		assayNote = {}
+
+		for r in self.results[2][1]:
+			key = r[keyCol]
+			if assayNote.has_key (key):
+				assayNote[key] = assayNote[key] + r[noteCol]
+			else:
+				assayNote[key] = r[noteCol]
+
+		logger.debug ('Found %d assay notes' % len(assayNote))
+		return assayNote
+
+	def findNamesIDs (self, results):
+		ids = {}
+		names = {}
+
+		columns, rows = results
+		prepCol = Gatherer.columnNumber (columns, '_ProbePrep_key')
+		nameCol = Gatherer.columnNumber (columns, 'name')
+		idCol = Gatherer.columnNumber (columns, 'accID')
+		
+		for r in rows:
+			key = r[prepCol]
+
+			# only keep the first ID and name found for
+			# each probe/antibody
+
+			if not ids.has_key(key):
+				ids[key] = r[idCol]
+				names[key] = r[nameCol] 
+		return ids, names
+
+	def findAssayResults (self, alleleSystemMap):
+
+		# preliminary extractions from queries 1-4
+
+		alleleComp, strain = self.findGenotypes()
+		assayNotes = self.findAssayNotes()
+		probeIDs, probeNames = self.findNamesIDs (self.results[3])
+		antibodyIDs, antibodyNames = self.findNamesIDs (
+			self.results[4])
+
+		# detailed assay results from query 5
+
+		cols = self.results[-1][0]
+
+		alleleCol = Gatherer.columnNumber (cols, '_Allele_key')
+		systemCol = Gatherer.columnNumber (cols, 'system')
+		structureCol = Gatherer.columnNumber (cols, 'structure')
+		assayTypeCol = Gatherer.columnNumber (cols, '_AssayType_key')
+		reporterCol = Gatherer.columnNumber (cols,
+			'_ReporterGene_key')
+		assayCol = Gatherer.columnNumber (cols, '_Assay_key')
+		ageCol = Gatherer.columnNumber (cols, 'age')
+		sexCol = Gatherer.columnNumber (cols, 'sex')
+		specimenNoteCol = Gatherer.columnNumber (cols, 'specimenNote')
+		genotypeCol = Gatherer.columnNumber (cols, '_Genotype_key')
+		resultNoteCol = Gatherer.columnNumber (cols, 'resultNote')
+		strengthCol = Gatherer.columnNumber (cols, '_Strength_key')
+		patternCol = Gatherer.columnNumber (cols, '_Pattern_key')
+		jnumCol = Gatherer.columnNumber (cols, 'jnumID')
+		prepCol = Gatherer.columnNumber (cols, '_ProbePrep_key')
+
+		out = []
+		columns = [ 'resultKey', 'alleleSystemKey', 'structure',
+			'age', 'sex', 'jnumID', 'resultNote', 'specimenNote',
+			'level', 'pattern', 'assayType', 'reporterGene',
+			'detectionMethod', 'allelicComposition', 'strain',
+			'assayNote', 'probeID', 'probeName', 'antibodyID',
+			'antibodyName',
+			]
+
+		i = 0
+		for r in self.results[-1][1]:
+			if not alleleSystemMap[r[alleleCol]].has_key (r[systemCol]):
+				logger.debug ('allele %d, missing "%s", systems: %s' % (r[alleleCol], r[systemCol], ', '.join(alleleSystemMap[r[alleleCol]].keys()) ) )
+			alleleSystemKey = \
+				alleleSystemMap[r[alleleCol]][r[systemCol]]
+
+			i = i + 1
+			row = [ i, alleleSystemKey, r[structureCol],
+				r[ageCol], r[sexCol], r[jnumCol],
+				r[resultNoteCol], r[specimenNoteCol], ]
+			row.append (Gatherer.resolve (r[strengthCol],
+				'GXD_Strength', '_Strength_key', 'strength'))
+			row.append (Gatherer.resolve (r[patternCol],
+				'GXD_Pattern', '_Pattern_key', 'pattern'))
+			row.append (Gatherer.resolve (r[assayTypeCol],
+				'GXD_AssayType', '_AssayType_key',
+				'assayType'))
+			row.append (Gatherer.resolve (r[reporterCol]))
+
+			prep = r[prepCol]
+			if probeIDs.has_key(prep):
+				row.append ('probe')
+			elif antibodyIDs.has_key(prep):
+				row.append ('antibody')
+			else:
+				row.append ('direct')
+
+			genotype = r[genotypeCol]
+
+			if alleleComp.has_key (genotype):
+				row.append (alleleComp[genotype])
+			else:
+				row.append (None)
+
+			if strain.has_key (genotype):
+				row.append (strain[genotype])
+			else:
+				row.append (None)
+
+			if assayNotes.has_key (r[assayCol]):
+				row.append (assayNotes[r[assayCol]])
+			else:
+				row.append (None)
+
+			toDo = [ probeIDs, probeNames, antibodyIDs,
+				antibodyNames ]
+
+			for dict in toDo:
+				if dict.has_key(prep):
+					row.append (dict[prep])
+				else:
+					row.append (None)
+
+			out.append (row)
+
+		logger.debug ('Found %d assay results' % len(out))
+		return columns, out
+
+	def findSortValues (self, cols, rows):
+		keyCol = Gatherer.columnNumber (cols, 'resultKey')
+
+		byFields = [ 'structure', 'age', 'level', 'pattern', 'jnumID',
+			'assayType', 'reporterGene', 'detectionMethod',
+			'assayNote', 'allelicComposition', 'sex',
+			'specimenNote', 'resultNote' ]
+
+		sorted = {}		# sorted[key] = [ field1, ... ]
+		for row in rows:
+			sorted[row[keyCol]] = [ row[keyCol] ]
+
+		for field in byFields:
+			nullValued = []
+			sortable = []
+
+			myCol = Gatherer.columnNumber (cols, field)
+			for row in rows:
+				myVal = row[myCol]
+				if myVal == None:
+					nullValued.append (row[keyCol])
+				else:
+					sortable.append ((myVal.lower(),
+						row[keyCol]))
+			sortable.sort()
+
+			i = 1
+			for (myValue, key) in sortable:
+				sorted[key].append (i)
+				i = i + 1
+
+			for key in nullValued:
+				sorted[key].append (i)
+
+			logger.debug ('Sorted %d rows by %s' % (i - 1, field))
+
+		allRows = sorted.values()
+		allRows.sort()
+		logger.debug ('Sorted output rows prior to load')
+
+		return [ 'resultKey' ] + byFields, allRows
+
+	def collateResults (self):
+
+		# step 1 -- recombinase_allele_system table
+
+		alleleSystemMap, alleleData, columns, rows = \
+			self.findAlleleSystemPairs()
+		self.output.append ( (columns, rows) )
+
+		# step 2 -- recombinase_other_system table
+
+		columns, rows = self.findOtherSystems (alleleSystemMap)
+		self.output.append ( (columns, rows) )
+
+		# step 3 -- recombinase_other_allele table
+
+		columns, rows = self.findOtherAlleles (alleleSystemMap,
+			alleleData)
+		self.output.append ( (columns, rows) )
+
+		# step 4 -- recombinase_assay_result table
+
+		columns, rows = self.findAssayResults (alleleSystemMap)
+		self.output.append ( (columns, rows) )
+
+		# step 5 -- recombinase_assay_result_sequence_num table
+
+		columns, rows = self.findSortValues (columns, rows)
+		self.output.append ( (columns, rows) )
+		return
+
+###--- globals ---###
+
+# SQL commands to be executed against the source database
+cmds = [
+	# all allele / system pairs; order by logical db to prioritize MGI IDs
+	'''select distinct c._Allele_key, a.accID, c.system, a._LogicalDB_key,
+		c.symbol
+	from all_cre_cache c,
+		acc_accession a
+	where c._Allele_key = a._Object_key
+		and a.preferred = 1
+		and a.private = 0
+		and a._MGIType_key = 11
+	order by c._Allele_key, a._LogicalDB_key''',
+
+	# genetic background info by genotype
+	'''select distinct s._Genotype_key, mnc.note, mnc.sequenceNum,
+		t.strain
+	from all_cre_cache c,
+		gxd_specimen s,
+		mgi_note mn,
+		mgi_notechunk mnc,
+		gxd_genotype g,
+		prb_strain t
+	where c._Assay_key = s._Assay_key
+		and s._Genotype_key = mn._Object_key
+		and mn._NoteType_key = 1018
+		and mn._Note_key = mnc._Note_key
+		and s._Genotype_key = g._Genotype_key
+		and g._Strain_key = t._Strain_key
+	order by mnc.sequenceNum''',
+
+	# assay notes by assay key
+	'''select distinct a._Assay_key, a.sequenceNum, a.assayNote
+	from gxd_assaynote a,
+		all_cre_cache c
+	where a._Assay_key = c._Assay_key
+	order by a.sequenceNum''',
+
+	# probes for each Cre probe prep key
+
+	'''select g._ProbePrep_key,
+		p.name,
+		acc.accID,
+		acc._LogicalDB_key
+	from all_cre_cache c,
+		gxd_assay a,
+		gxd_probeprep g,
+		prb_probe p,
+		acc_accession acc
+	where c._Assay_key = a._Assay_key
+		and a._ProbePrep_key = g._ProbePrep_key
+		and g._Probe_key = p._Probe_key
+		and g._Probe_key = acc._Object_key
+		and acc._MGIType_key = 3
+		and acc.preferred = 1
+		and acc.private = 0
+	order by acc._LogicalDB_key''',		# MGI IDs preferred
+
+	# antibodies for each Cre antibody key
+
+	'''select g._AntibodyPrep_key as _ProbePrep_key,
+		p.antibodyName as name,
+		acc.accID,
+		acc._LogicalDB_key
+	from all_cre_cache c,
+		gxd_assay a,
+		gxd_antibodyprep g,
+		gxd_antibody p,
+		acc_accession acc
+	where c._Assay_key = a._Assay_key
+		and a._AntibodyPrep_key = g._AntibodyPrep_key
+		and g._Antibody_key = p._Antibody_key
+		and g._Antibody_key = acc._Object_key
+		and acc._MGIType_key = 6
+		and acc.preferred = 1
+		and acc.private = 0
+	order by acc._LogicalDB_key''',		# MGI IDs preferred
+
+	# main cre assay result data
+	'''select c._Allele_key, c.system, c.structure,
+		a._AssayType_key, a._ReporterGene_key, a._Refs_key,
+		a._Assay_key, a._ProbePrep_key,
+		s.age, s.sex, s.specimenNote, s._Genotype_key,
+		r.resultNote, r._Strength_key, r._Pattern_key,
+		b.jnumID
+	from all_cre_cache c,
+		gxd_assay a,
+		gxd_specimen s,
+		gxd_insituresult r,
+		bib_citation_cache b
+	where c._Assay_key = a._Assay_key
+		and a._Assay_key = s._Assay_key
+		and s._Specimen_key = r._Specimen_key
+		and a._Refs_key = b._Refs_key''',
+	]
+
+# data about files to be written; for each:  (filename prefix, list of field
+#	names in order to be written, name of table to be loaded)
+files = [
+	('recombinase_allele_system',
+		[ 'alleleSystemKey', 'alleleKey', 'alleleID', 'system' ],
+		'recombinase_allele_system'),
+
+	('recombinase_other_system',
+		[ 'uniqueKey', 'alleleSystemKey', 'system' ],
+		'recombinase_other_system'),
+
+	('recombinase_other_allele',
+		[ 'uniqueKey', 'alleleSystemKey', 'alleleKey', 'alleleID',
+			'alleleSymbol', ],
+		'recombinase_other_allele'),
+
+	('recombinase_assay_result',
+		[ 'resultKey', 'alleleSystemKey', 'structure', 'age', 'level',
+			'pattern', 'jnumID', 'assayType', 'reporterGene',
+			'detectionMethod', 'sex', 'allelicComposition',
+			'strain', 'assayNote', 'resultNote',
+			'specimenNote', 'probeID', 'probeName', 'antibodyID',
+			'antibodyName', ],
+		'recombinase_assay_result'),
+
+	('recombinase_assay_result_sequence_num',
+		[ 'resultKey', 'structure', 'age', 'level', 'pattern',
+			'jnumID', 'assayType', 'reporterGene',
+			'detectionMethod', 'assayNote', 'allelicComposition',
+			'sex', 'specimenNote', 'resultNote' ],
+		'recombinase_assay_result_sequence_num'),
+	]
+
+# global instance of a RecombinaseGatherer
+gatherer = RecombinaseGatherer (files, cmds)
+
+###--- main program ---###
+
+# if invoked as a script, use the standard main() program for gatherers and
+# pass in our particular gatherer
+if __name__ == '__main__':
+	Gatherer.main (gatherer)
