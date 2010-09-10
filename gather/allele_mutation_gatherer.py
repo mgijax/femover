fb7494dc7591ad0d5617c530031be904187377a8
Jonathan Beal <Jon.Beal@jax.org>
2010-09-10 11:20:33 +0000

    initial addition

---
+++ gather/allele_mutation_gatherer.py
@@ -0,0 +1,37 @@
+#!/usr/local/bin/python
+# 
+# gathers data for the 'allele_mutation' table in the front-end database
+
+import Gatherer
+
+###--- Classes ---###
+
+AlleleMutationGatherer = Gatherer.Gatherer
+	# Is: a data gatherer for the allele_mutation table
+	# Has: queries to execute against the source database
+	# Does: queries the source database for mutations for alleles,
+	#	collates results, writes tab-delimited text file
+
+###--- globals ---###
+
+cmds = [ '''select m._Allele_key, t.term
+		from all_allele_mutation m, voc_term t
+		where m._Mutation_key = t._Term_key'''
+	]
+
+# order of fields (from the query results) to be written to the
+# output file
+fieldOrder = [ Gatherer.AUTO, '_Allele_key', 'term' ]
+
+# prefix for the filename of the output file
+filenamePrefix = 'allele_mutation'
+
+# global instance of a AlleleMutationGatherer
+gatherer = AlleleMutationGatherer (filenamePrefix, fieldOrder, cmds)
+
+###--- main program ---###
+
+# if invoked as a script, use the standard main() program for gatherers and
+# pass in our particular gatherer
+if __name__ == '__main__':
+	Gatherer.main (gatherer)
---
+++ schema/term_sibling.py
@@ -0,0 +1,43 @@
+#!/usr/local/bin/python
+
+import Table
+
+# contains data definition information for the term_sibling table
+
+# Note: All table and field names should be all-lowercase with underscores
+# used to separate words.
+
+###--- Globals ---###
+
+# name of this database table
+tableName = 'term_sibling'
+
+# MySQL statement to create this table
+createStatement = '''CREATE TABLE %s  ( 
+	unique_key		int		not null,
+	term_key		int		not null,
+	sibling_term_key	int		not null,
+	sibling_term		varchar(255)	null,
+	sibling_primary_id	varchar(30)	null,
+	sequence_num		int		null,
+	is_leaf			int		not null,
+	edge_label		varchar(255)	null,
+	path_number		int		not null,
+	PRIMARY KEY(unique_key))''' % tableName
+
+# Maps from index suffix to create statement for that index.  In each
+# statement, the first %s is for the index name, and the second is for the
+# table name.
+indexes = {
+	'term_key' : 'create index %s on %s (term_key)',
+	}
+
+# global instance of this Table object
+table = Table.Table (tableName, createStatement, indexes)
+
+###--- Main program ---###
+
+# if executed as a script, pass the global Table object into the general
+# main program for Table subclasses
+if __name__ == '__main__':
+	Table.main(table)
