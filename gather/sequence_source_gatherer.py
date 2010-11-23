af90c5b97215c5da86c14cb9cf7375073848fea9
Jonathan Beal <Jon.Beal@jax.org>
2010-11-23 16:44:51 +0000

    added handling for raw values for strain, tissue, age, sex, and cell line

--- gather/sequence_source_gatherer.py
+++ gather/sequence_source_gatherer.py
@@ -5,6 +5,23 @@
 import config
 import Gatherer
 
+###--- Global Constants ---###
+
+STRAIN = 'rawStrain'		# constants for fields from SEQ_Sequence_Raw
+TISSUE = 'rawTissue'
+AGE = 'rawAge'
+SEX = 'rawSex'
+CELLLINE = 'rawCellLine'
+
+NOT_SPECIFIED = 'Not Specified'
+
+###--- Functions ---###
+
+def notNone (s):
+	if s == None:
+		return ''
+	return s
+
 ###--- Classes ---###
 
 class SequenceSourceGatherer (Gatherer.ChunkGatherer):
@@ -13,25 +30,105 @@ class SequenceSourceGatherer (Gatherer.ChunkGatherer):
 	# Does: queries source database for source data for sequences,
 	#	collates results, writes tab-delimited text file
 
+	def collateResults (self):
+		# Purpose: we override this method to provide handling for
+		#	fields where we need raw values
+
+		# collect the raw values for five columns into a dictionary
+		# keyed by sequence key, where each value is a dictionary
+		# keyed by the global constants for the fields
+
+		# rawValues[seq key] = { fieldname : raw value }
+		self.rawValues = {}
+
+		columns = self.results[0][0]
+		keyCol = Gatherer.columnNumber (columns, '_Sequence_key')
+		strainCol = Gatherer.columnNumber (columns, STRAIN)
+		tissueCol = Gatherer.columnNumber (columns, TISSUE)
+		ageCol = Gatherer.columnNumber (columns, AGE)
+		sexCol = Gatherer.columnNumber (columns, SEX)
+		cellLineCol = Gatherer.columnNumber (columns, CELLLINE)
+
+		for row in self.results[0][1]:
+			self.rawValues[row[keyCol]] = {
+				STRAIN : row[strainCol],
+				TISSUE : row[tissueCol],
+				AGE : row[ageCol],
+				SEX : row[sexCol],
+				CELLLINE : row[cellLineCol]
+				}
+
+		self.finalColumns = self.results[-1][0]
+		self.finalResults = self.results[-1][1]
+		return
+
 	def postprocessResults (self):
 		# Purpose: we override this method to provide cached key
 		#	lookups, to attempt to give a performance boost
 
 		self.convertFinalResultsToList()
 
+		keyCol = Gatherer.columnNumber (self.finalColumns,
+			'_Sequence_key')
 		tissueCol = Gatherer.columnNumber (self.finalColumns,
 			'_Tissue_key')
 		sexCol = Gatherer.columnNumber (self.finalColumns,
 			'_Gender_key')
+		strainCol = Gatherer.columnNumber (self.finalColumns,
+			'strain')
+		ageCol = Gatherer.columnNumber (self.finalColumns,
+			'age')
+		cellLineCol = Gatherer.columnNumber (self.finalColumns,
+			'cellLine')
 
 		for r in self.finalResults:
 
+			seqKey = r[keyCol]
+			if self.rawValues.has_key(seqKey):
+				raw = self.rawValues[seqKey]
+			else:
+				raw = None
+
 			# lookups from voc_term
 
-			self.addColumn ('tissue', Gatherer.resolve(
-				r[tissueCol]), r, self.finalColumns)
-			self.addColumn ('sex', Gatherer.resolve(
-				r[sexCol]), r, self.finalColumns)
+			tissue = Gatherer.resolve(r[tissueCol])
+			sex = Gatherer.resolve(r[sexCol])
+
+			# for any unspecified or unresolved items, update
+			# them with the raw values
+
+			if raw:
+				toReplace = [ None, '', 'Not Resolved' ]
+
+				if tissue in toReplace:
+					tissue = notNone(raw[TISSUE]) + '*'
+					if tissue == '*':
+						tissue = NOT_SPECIFIED
+
+				if sex in toReplace:
+					sex = notNone(raw[SEX]) + '*'
+					if sex == '*':
+						sex = NOT_SPECIFIED
+
+				if r[strainCol] in toReplace:
+					r[strainCol] = \
+						notNone(raw[STRAIN]) + '*'
+					if r[strainCol] == '*':
+						r[strainCol] = NOT_SPECIFIED
+
+				if r[ageCol] in toReplace:
+					r[ageCol] = notNone(raw[AGE]) + '*'
+					if r[ageCol] == '*':
+						r[ageCol] = NOT_SPECIFIED
+
+				if r[cellLineCol] in toReplace:
+					r[cellLineCol] = \
+						notNone(raw[CELLLINE]) + '*'
+					if r[cellLineCol] == '*':
+						r[cellLineCol] = NOT_SPECIFIED
+
+			self.addColumn('tissue', tissue, r, self.finalColumns)
+			self.addColumn('sex', sex, r, self.finalColumns)
 		return
 
 	def getMinKeyQuery (self):
@@ -43,20 +140,25 @@ class SequenceSourceGatherer (Gatherer.ChunkGatherer):
 ###--- globals ---###
 
 cmds = [
+	'''select _Sequence_key, rawStrain, rawTissue, rawAge, rawSex,
+		rawCellLine
+	from seq_sequence_raw
+	where _Sequence_key >= %d and _Sequence_key < %d''',
+
 	'''select ssa._Sequence_key,
 		s.strain,
 		ps._Tissue_key,
 		ps._Gender_key,
 		ps.age,
-		c.cellLine
+		c.term as cellLine
 	from seq_source_assoc ssa,
 		prb_source ps,
-		all_cellline c,
+		voc_term c,
 		prb_strain s
 	where ssa._Sequence_key >= %d and ssa._Sequence_key < %d
 		and ssa._Source_key = ps._Source_key
 		and ps._Strain_key = s._Strain_key
-		and ps._CellLine_key = c._CellLine_key'''
+		and ps._CellLine_key = c._Term_key'''
 	]
 
 # order of fields (from the query results) to be written to the
