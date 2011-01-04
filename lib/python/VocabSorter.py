c5f4ca38f4db6a96e8a64c20836ddfcfad5a91cc
Jonathan Beal <Jon.Beal@jax.org>
2011-01-04 15:51:04 +0000

    initial addition

---
+++ lib/python/VocabSorter.py
@@ -0,0 +1,267 @@
+# Module: VocabSorter.py
+# Purpose: to provide sorting of vocabulary terms, using a depth-first search
+#	(DFS) algorithm within DAG-based vocabularies.  For sibling terms, we
+#	prioritize them by its sequence number from the database (if
+#	available) or alphabetically if not.  Each term is given its new
+#	sequence number when it is first visited by the DFS algorithm, so in
+#	the case of a term with multiple parent nodes, only the first one
+#	affects its ordering.  For flat vocabularies, the terms are ordered as
+#	for siblings of a DAG-based vocabulary (the sequence number from the
+#	database, then alphabetically).
+
+import dbAgnostic
+import logger
+
+###--- Globals ---###
+
+VOCAB_SORTER = None		# singleton instance to be shared
+
+###--- PrivateFunctions ---###
+
+def __initialize():
+	global VOCAB_SORTER
+	if not VOCAB_SORTER:
+		VOCAB_SORTER = VocabSorter()
+	return
+
+def _columnNumber (columns, columnName):
+	if columnName in columns:
+		return columns.index(columnName)
+	c = columnName.lower()
+	return columns.index(c)
+
+###--- Classes ---###
+
+class VocabSorter:
+	def __init__ (self):
+		# maps from (vocab name, term) to the respective term key
+		self.termKeys = {}
+
+		# maps from term key to the term's default ordering
+		self.defaultOrder = {}
+
+		# maps from term key to list of its descendent term keys
+		self.children = {}
+
+		# dictionary of term keys for root terms
+		self.roots = {}
+
+		# dictionary of term keys which are not root terms
+		self.notRoots = {}
+
+		# maps from term keys to their final, DAG-considered ordering
+		self.finalOrder = {}
+
+		# load from database
+		self.__getTermKeysAndDefaultOrder()
+		self.__getDAGs()
+
+		# order the children of each parent node
+		self.__orderChildren()
+		
+		# compute the final ordering, using a DFS algorithm on the DAG
+		# vocabularies
+		self.__computeFinalOrder()
+		return
+
+	###--- public methods ---###
+
+	def sequenceNum (self, termKey):
+		if self.finalOrder.has_key(termKey):
+			return self.finalOrder[termKey]
+		return None
+
+	def getTermKey (self, vocabName, term):
+		if self.termKeys.has_key ( (vocabName, term) ):
+			return self.termKeys ( (vocabName, term) )
+		return None
+
+	###--- private methods ---###
+
+	def __getTermKeysAndDefaultOrder (self):
+		# all vocabulary terms with their pre-assigned sequence
+		# numbers and their vocabulary names
+
+		cmd = '''select t._Term_key,
+				t.term, 
+				t.sequenceNum,
+				v.name
+			from voc_term t,
+				voc_vocab v
+			where t._Vocab_key = v._Vocab_key'''
+
+		# retrieve terms from the database
+
+		(cols, rows) = dbAgnostic.execute (cmd)
+		logger.debug ('Retrieved %d terms from database' % len(rows))
+
+		# order all terms within a vocabulary by their pre-assigned
+		# sequence number from the database, then alphabetically
+
+		keyCol = _columnNumber (cols, '_Term_key')
+		termCol = _columnNumber (cols, 'term')
+		seqCol = _columnNumber (cols, 'sequenceNum')
+		vocabCol = _columnNumber (cols, 'name')
+
+		termList = []
+		for row in rows:
+			term = row[termCol]
+			if term:
+				term = term.lower()
+			item = (row[vocabCol],
+				row[seqCol],
+				term,
+				row[keyCol])
+			termList.append (item)
+		termList.sort()
+
+		i = 0
+		for (vocab, seqNum, term, termKey) in termList:
+			i = i + 1
+			self.defaultOrder[termKey] = i
+		logger.debug ('Computed default ordering')
+
+		# map (vocabulary name, term) to the respective term keys
+
+		for row in rows:
+			self.termKeys[(row[vocabCol], row[termCol])] = \
+				row[keyCol]
+		logger.debug ('Mapped vocab/term pairs to term keys')
+		return
+
+	def __getDAGs (self):
+		# all parent/child relationships for vocab terms (MGIType 13)
+
+		cmd = '''select distinct p._Object_key as parentKey,
+				c._Object_key as childKey
+			from dag_dag d,
+				dag_edge e,
+				dag_node p,
+				dag_node c
+			where d._MGIType_key = 13
+				and d._DAG_key = e._DAG_key
+				and e._Parent_key = p._Node_key
+				and e._Child_key = c._Node_key'''
+
+		(cols, rows) = dbAgnostic.execute (cmd)
+		logger.debug ('Retrieved %d relationships from database' % \
+			len(rows))
+
+		childCol = _columnNumber (cols, 'childkey')
+		parentCol = _columnNumber (cols, 'parentkey')
+
+		for row in rows:
+			child = row[childCol]
+			parent = row[parentCol]
+
+			# we know that a child node is not a root node
+			self.notRoots[child] = 1
+
+			# if previously flagged as a root, remove the flag
+			if self.roots.has_key(child):
+				del self.roots[child]
+
+			# unless we have seen the parent node as a child
+			# previously, then it is a root as far as we know now
+			if not self.notRoots.has_key(parent):
+				self.roots[parent] = 1
+
+			# add the child to the list of the parent's children
+			if self.children.has_key(parent):
+				self.children[parent].append (child)
+			else:
+				self.children[parent] = [ child ]
+
+		logger.debug ('Found %d roots, %d descendents' % (
+			len(self.roots), len(self.notRoots)) )
+		return
+
+	def __orderChildren (self):
+		# orders the children of each parent node according to their
+		# default ordering
+
+		parentKeys = self.children.keys()
+		for parent in parentKeys:
+			self.children[parent] = self.__orderTermList (
+				self.children[parent])
+
+		logger.debug ('Ordered children of %d parents' % \
+			len(parentKeys))
+		return
+
+	def __orderTermList (self, siblings):
+		siblist = []
+		for sibling in siblings:
+			siblist.append ((self.defaultOrder[sibling], sibling))
+		siblist.sort()
+
+		# pull the now-ordered term keys back out of 'siblist'
+		return map (lambda y : y[1], siblist)
+
+	def __computeFinalOrder (self):
+		# order the terms using a DFS algorithm to traverse down the
+		# DAGs.  fill in ordering for any non-DAG vocabulary terms as
+		# well.  This method fills in self.finalOrder.
+
+		# last-in-first-out structure holding lists of terms to come
+		# back and process.  stack[-1] is the most-recent list of
+		# siblings that we need to come back and process; once all of
+		# those are done, we remove that sublist and begin on the new
+		# stack[-1], etc.
+		stack = []
+
+		# get the ordered list of roots for the DAGs.  Any terms not
+		# reachable from those in 'rootList' are from non-DAG
+		# vocabularies; we will come back and order those ones later.
+		rootList = self.__orderTermList(self.roots.keys())
+		if rootList:
+			stack.append (rootList)
+
+		# counter for ordering terms as they are encountered
+		seqNum = 0
+
+		while stack:
+			# pop the first item off the sublist at the top of the
+			# stack
+			toDo = stack[-1][0]
+			del stack[-1][0]
+
+			# if that leaves an empty sublist at the top of the
+			# stack, remove it
+			if len(stack[-1]) == 0:
+				del stack[-1]
+
+			# if we have not ordered this term, then note its
+			# order
+			if not self.finalOrder.has_key(toDo):
+				seqNum = seqNum + 1
+				self.finalOrder[toDo] = seqNum
+
+			# look up the children of this term and add them to
+			# the stack
+			if self.children.has_key(toDo):
+				stack.append (self.children[toDo][:])
+
+		logger.debug ('Ordered %d DAG-based terms' % seqNum)
+		dag = seqNum
+
+		# now we need to fill in the ordering for any terms that were
+		# not in the DAG-based vocabularies
+
+		termKeys = self.__orderTermList (self.defaultOrder.keys())
+		for term in termKeys:
+			if not self.finalOrder.has_key(term):
+				seqNum = seqNum + 1
+				self.finalOrder[term] = seqNum
+		logger.debug ('Ordered %d flat vocab terms' % (seqNum - dag))
+		return
+
+###--- Public Functions ---###
+
+def getSequenceNum (termKey):
+	__initialize()
+	return VOCAB_SORTER.sequenceNum (termKey)
+
+def getSequenceNumByTerm (vocabName, term):
+	__initialize()
+	return getSequenceNum (VOCAB_SORTER.getTermKey (vocabName, term))
---
+++ lib/python/dbAgnostic.py
@@ -0,0 +1,86 @@
+#!/usr/local/bin/python
+# 
+# provides an execute() method for executing queries against Sybase, MySQL, or
+#	Postgres as a source database.  Ensures that the caller does not need
+#	to know which is which.
+
+import config
+import logger
+import dbManager
+
+###--- Globals ---###
+
+Error = 'dbAgnostic.error'	# exception raised by this module
+SOURCE_DB = config.SOURCE_TYPE	# either sybase, mysql, or postgres
+DBM = None			# dbManager object for postgres/mysql access
+LOADED_SYBASE = False
+
+# set up our database connectivity
+
+if SOURCE_DB == 'sybase':
+	# Where possible, use the FreeTDS library for Sybase access.  If not
+	# possible, use our MGI db.py interface to Sybase's client libraries.
+	try:
+		import freedb
+		db = freedb
+		logger.debug ('Loaded FreeTDS freedb library')
+	except:
+		import db
+		db.set_sqlLogin (config.SOURCE_USER, config.SOURCE_PASSWORD,
+			config.SOURCE_HOST, config.SOURCE_DATABASE)
+		logger.debug ('Loaded MGI-written db library')
+
+	LOADED_SYBASE = True
+	db.useOneConnection(1)
+
+elif SOURCE_DB == 'postgres':
+	DBM = dbManager.postgresManager (config.SOURCE_HOST,
+		config.SOURCE_DATABASE, config.SOURCE_USER,
+		config.SOURCE_PASSWORD)
+	logger.debug ('Created postgresManager')
+
+elif SOURCE_DB == 'mysql':
+	DBM = dbManager.mysqlManager (config.SOURCE_HOST,
+		config.SOURCE_DATABASE, config.SOURCE_USER,
+		config.SOURCE_PASSWORD)
+	logger.debug ('Created mysqlManager')
+else:
+	raise Error, 'Unknown value for config.SOURCE_TYPE : %s' % SOURCE_DB
+
+###--- Functions ---###
+
+def execute (cmd):
+	# Purpose: execute the given SQL 'cmd' against the source database
+	# Returns: two-item tuple of (columns, rows)
+	#	columns - list of column names
+	#	rows - list of rows, where each row is a list of values in the
+	#		order corresponding to the column names in 'columns'
+
+	# if using either postgres or mysql, our dbManager object will handle
+	# all necessary database interaction
+
+	if DBM:
+		return DBM.execute(cmd)
+
+	# if not using a dbManager (ie- we are querying Sybase), then convert
+	# the Sybase-style return to the dbManager-style return
+
+	if not LOADED_SYBASE:
+		raise Error, 'Failed to load Sybase driver'
+
+	results = db.sql (cmd, 'auto')
+	columns = []
+	rows = []
+
+	if results:
+		columns = results[0].keys()
+		columns.sort()
+
+		for sybRow in results:
+			row = []
+			for col in columns:
+				row.append (sybRow[col])
+			rows.append (row)
+
+	return columns, rows
+
