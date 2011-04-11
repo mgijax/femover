f396b16f0b6d8485647b1a56782f0006451fce4a
Jonathan Beal <Jon.Beal@jax.org>
2011-04-11 19:33:32 +0000

    split out search_id and display_id

--- schema/accession.py
+++ schema/accession.py
@@ -16,7 +16,8 @@ tableName = 'accession'
 createStatement = '''CREATE TABLE %s  ( 
 	unique_key		int		not null,
 	object_key		int		not null,
-	acc_id			varchar(30)	not null,
+	search_id		varchar(30)	not null,
+	display_id		varchar(30)	not null,
 	sequence_num		smallint	not null,
 	description		varchar(315)	null,
 	logical_db_key		smallint	not null,
@@ -28,8 +29,8 @@ createStatement = '''CREATE TABLE %s  (
 # statement, the first %s is for the index name, and the second is for the
 # table name.
 indexes = {
-	'lower_acc_id' : 'create index %s on %s (lower(acc_id))',
-	'acc_id' : 'create index %s on %s (acc_id)',
+	'lower_acc_id' : 'create index %s on %s (lower(search_id))',
+	'acc_id' : 'create index %s on %s (search_id)',
 	}
 
 # global instance of this Table object
