61bb887fe187f541e0b92e6ff260b2113a59f8e4
Jonathan Beal <Jon.Beal@jax.org>
2011-08-16 18:26:02 +0000

    broadened the number of markers included

--- gather/marker_gatherer.py
+++ gather/marker_gatherer.py
@@ -15,6 +15,7 @@ class MarkerGatherer (Gatherer.Gatherer):
 
 	def collateResults (self):
 		self.featureTypes = {}	# marker key -> [ feature types ]
+		self.ids = {}		# marker key -> (id, logical db key)
 
 		# feature types from MCV vocab are in query 0 results
 
@@ -34,9 +35,24 @@ class MarkerGatherer (Gatherer.Gatherer):
 		logger.debug ('Found %d MCV terms for %d markers' % (
 			len(rows), len(self.featureTypes) ) )
 
+		# IDs are retrieved in query 1
+
+		cols, rows = self.results[1]
+		keyCol = Gatherer.columnNumber (cols, '_Marker_key')
+		idCol = Gatherer.columnNumber (cols, 'accid')
+		ldbCol = Gatherer.columnNumber (cols, '_LogicalDB_key')
+
+		for row in rows:
+			self.ids[row[keyCol]] = (row[idCol], row[ldbCol])
+	
+		logger.debug ('Found %d primary IDs for markers' % \
+			len(self.ids))
+
 		# last query has the bulk of the data
 		self.finalColumns = self.results[-1][0]
 		self.finalResults = self.results[-1][1]
+
+		logger.debug ('Found %d markers' % len(self.finalResults))
 		return
 
 	def postprocessResults (self):
@@ -46,8 +62,6 @@ class MarkerGatherer (Gatherer.Gatherer):
 
 		statusCol = Gatherer.columnNumber (self.finalColumns,
 			'_Marker_Status_key')
-		ldbCol = Gatherer.columnNumber (self.finalColumns,
-			'_LogicalDB_key')
 		typeCol = Gatherer.columnNumber (self.finalColumns,
 			'_Marker_Type_key')
 		orgCol = Gatherer.columnNumber (self.finalColumns,
@@ -63,15 +77,26 @@ class MarkerGatherer (Gatherer.Gatherer):
 			else:
 				feature = None
 
+			# look up cached ID and logical database
+			if self.ids.has_key(markerKey):
+				accid, ldb = self.ids[markerKey]
+				ldbName = Gatherer.resolve (ldb,
+					'acc_logicaldb', '_LogicalDB_key',
+					'name')
+			else:
+				accid = None
+				ldb = None
+				ldbName = None
+
+			self.addColumn ('accid', accid, r, self.finalColumns)
 			self.addColumn ('subtype', feature, r,
 				self.finalColumns)
 			self.addColumn ('status', Gatherer.resolve (
 				r[statusCol], 'mrk_status',
 				'_Marker_Status_key', 'status'),
 				r, self.finalColumns)
-			self.addColumn ('logicalDB', Gatherer.resolve (
-				r[ldbCol], 'acc_logicaldb', '_LogicalDB_key',
-				'name'), r, self.finalColumns)
+			self.addColumn ('logicalDB', ldbName, r,
+				self.finalColumns)
 			self.addColumn ('markerType', Gatherer.resolve (
 				r[typeCol], 'mrk_types', '_Marker_Type_key',
 				'name'), r, self.finalColumns)
@@ -87,16 +112,11 @@ cmds = [
 	# the subtype for each marker
 	'select distinct _Marker_key, directTerms from MRK_MCV_Cache',
 
-	# 1. Gather the list of valid mouse markers for this report
+	# 1. Gather the set of primary marker IDs
 	# (mouse markers on the top of the union, non-mouse markers below)
 	'''select m._Marker_key,
-		m.symbol,
-		m.name, 
-		m._Marker_Type_key,
-		m._Organism_key,
 		a.accID, 
-		a._LogicalDB_key,
-		m._Marker_Status_key
+		a._LogicalDB_key
 	from mrk_marker m,
 		acc_accession a
 	where m._Marker_key = a._Object_key
@@ -106,13 +126,8 @@ cmds = [
 		and m._Organism_key = 1
 	union
 	select m._Marker_key,
-		m.symbol,
-		m.name, 
-		m._Marker_Type_key,
-		m._Organism_key,
 		a.accID, 
-		a._LogicalDB_key,
-		m._Marker_Status_key
+		a._LogicalDB_key
 	from mrk_marker m,
 		acc_accession a,
 		acc_logicaldb ldb
@@ -120,11 +135,13 @@ cmds = [
 		and a._MGIType_key = 2
 		and m._Organism_key != 1
 		and a.preferred = 1
-		and a._LogicalDB_key != 4
 		and a._LogicalDB_key = ldb._LogicalDB_key
 		and ldb._Organism_key = m._Organism_key''',
-	# exclude RapMap from rat marker IDs (logical database 4)
-	# There are 15 rat markers that will be excluded because of this.
+
+	# all markers
+	'''select _Marker_key, symbol, name, _Marker_Type_key, _Organism_key,
+		_Marker_Status_key
+	from mrk_marker''',
 	]
 
 # order of fields (from the Sybase query results) to be written to the
