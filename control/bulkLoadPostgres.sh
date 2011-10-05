a3f6b3870173240c0715de08381b268193427a59
Jonathan Beal <Jon.Beal@jax.org>
2011-10-05 17:07:53 +0000

    updated to use LOG_DIR variable

--- control/bulkLoadPostgres.sh
+++ control/bulkLoadPostgres.sh
@@ -3,15 +3,17 @@
 # bulk load an input file into a Postgres table
 # Usage: bulkLoadPostgres.sh <server> <database> <user> <password> <input file> <table name>
 
+. ../Configuration
+
 PGPASSWORD=$4
 export PGPASSWORD
 
-psql -h $1 -d $2 -U $3 --command "\copy $6 from '$5' with null as ''" > ../logs/$6.load.log 2>&1
+psql -h $1 -d $2 -U $3 --command "\copy $6 from '$5' with null as ''" > ${LOG_DIR}/$6.load.log 2>&1
 EXITCODE=$?
 
 if [ ${EXITCODE} -ne 0 ]; then
 	exit ${EXITCODE}
 fi
 
-psql -h $1 -d $2 -U $3 --command "vacuum analyze $6;" > ../logs/$6.vacuum.log 2>&1
+psql -h $1 -d $2 -U $3 --command "vacuum analyze $6;" > ${LOG_DIR}/$6.vacuum.log 2>&1
 exit $?
