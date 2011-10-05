13d91c71df94907619c01344ba230909145c6c10
Jonathan Beal <Jon.Beal@jax.org>
2011-10-05 18:29:34 +0000

    initial addition

---
+++ control/reportMgiDbInfo.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+cd `dirname $0`; . ../Configuration
+
+usage()
+{
+    echo "Usage: reportMgiDbInfo.sh {postgres | mysql | sybase}"
+    echo "  Report data from the MGI_dbInfo table in the given source database"
+
+    if [ "$1" != "" ]; then
+	    echo "Error: $1"
+    fi
+
+    exit 1
+}
+
+# check that we have at least one parameter
+
+if [ $# -lt 1 ]; then
+	usage "Must specify a source database type"
+fi
+
+# handle the first parameter -- database type from which to read
+
+if [ "$1" = "postgres" ]; then
+	SOURCE_TYPE=postgres
+elif [ "$1" = "mysql" ]; then
+	SOURCE_TYPE=mysql
+elif [ "$1" = "sybase" ]; then
+	SOURCE_TYPE=sybase
+else
+	usage "Invalid database type"
+fi
+export SOURCE_TYPE
+
+# run the Python script to do the reporting
+${FEMOVER}/control/reportMgiDbInfo.py
+
+if [ $? -ne 0 ]; then
+	echo "Failed to read from MGI_dbInfo in $1 database"
+	exit 1
+fi
