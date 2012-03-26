a36c5f7ecb0b089a9a3dc0fe6adf0ae0d521d1ca
Jonathan Beal <Jon.Beal@jax.org>
2012-03-26 19:26:53 +0000

    added explicit, custom sorting for orthologous markers

--- gather/marker_orthology_gatherer.py
+++ gather/marker_orthology_gatherer.py
@@ -3,6 +3,7 @@
 # gathers data for the 'markerOrthology' table in the front-end database
 
 import Gatherer
+import logger
 
 ###--- Classes ---###
 
@@ -12,18 +13,72 @@ class MarkerOrthologyGatherer (Gatherer.Gatherer):
 	# Does: queries the source database for primary data for orthologies,
 	#	collates results, writes tab-delimited text file
 
-	def postprocessResults (self):
-		# Purpose: override to provide key-based lookups
+	def collateResults (self):
+		cols, rows = self.results[0]
 
-		self.convertFinalResultsToList()
+		mouseKeyCol = Gatherer.columnNumber (cols, 'mouseMarkerKey')
+		otherKeyCol = Gatherer.columnNumber (cols, 'otherMarkerKey')
+		otherSymbolCol = Gatherer.columnNumber (cols, 'otherSymbol')
+		organismCol = Gatherer.columnNumber (cols, 'commonName')
+		organismKeyCol = Gatherer.columnNumber (cols, '_Organism_key')
 
-		orgCol = Gatherer.columnNumber (self.finalColumns,
-			'_Organism_key')
-		for r in self.finalResults:
-			self.addColumn ('otherOrganism', Gatherer.resolve (
-				r[orgCol], 'mgi_organism',
-				'_Organism_key', 'commonName'), r,
-				self.finalColumns)
+		# byMouse[mouse marker key] -> [ [ priority value,
+		#	other organism (lowercased), other organism,
+		#	other marker key, other symbol ], ... ]
+		byMouse = {}
+
+		for row in rows:
+			mouseKey = row[mouseKeyCol]
+			otherOrganismKey = row[organismKeyCol]
+			otherOrganism = row[organismCol]
+
+			# To bring human and rat to the front of the list, we
+			# use a smaller priority value.  Others are grouped
+			# with the same priority value so they will be sorted
+			# based on lowercased organism name.
+
+			if otherOrganismKey == 1:
+				priority = 0	# mouse -- should not happen
+			elif otherOrganismKey == 2:
+				priority = 1	# human
+			elif otherOrganismKey == 40:
+				priority = 2	# rat
+			else:
+				priority = 3	# all other organisms
+
+			sortableRow = [ priority, otherOrganism.lower(),
+				otherOrganism, row[otherKeyCol],
+				row[otherSymbolCol] ]
+
+			if byMouse.has_key(mouseKey):
+				byMouse[mouseKey].append (sortableRow)
+			else:
+				byMouse[mouseKey] = [ sortableRow ]
+
+		logger.debug ('Processed %d orthology rows' % len(rows))
+
+		# now sort the rows for each marker and add them to the list
+		# of output rows
+
+		self.finalColumns = [ 'mouseMarkerKey', 'otherMarkerKey',
+			'otherSymbol', 'otherOrganism', 'sequenceNum' ]
+		self.finalResults = []
+
+		keys = byMouse.keys()
+		keys.sort()
+
+		for key in keys:
+			# sort the rows for this marker
+			byMouse[key].sort()
+
+			i = 0		# each marker gets its own counter
+
+			for row in byMouse[key]:
+				i = i + 1
+				self.finalResults.append ( [ key,
+					row[3], row[4], row[2], i ] )
+		logger.debug ('Got %d orthology rows for %d markers' % (
+			len(self.finalResults), len(byMouse)) )
 		return
 
 ###--- globals ---###
@@ -32,15 +87,18 @@ cmds = [
 	'''select distinct mouse._Marker_key as mouseMarkerKey,
 		nonmouse._Marker_key as otherMarkerKey,
 		mm.symbol as otherSymbol,
-		nonmouse._Organism_key
+		nonmouse._Organism_key,
+		org.commonName
 	from mrk_homology_cache mouse,
 		mrk_homology_cache nonmouse,
-		mrk_marker mm
+		mrk_marker mm,
+		mgi_organism org
 	where mouse._Organism_key = 1
 		and mouse._Class_key = nonmouse._Class_key
 		and nonmouse._Organism_key != 1
 		and exists (select 1 from mrk_marker m
 			where m._Marker_key = nonmouse._Marker_key)
+		and nonmouse._Organism_key = org._Organism_key
 		and nonmouse._Marker_key = mm._Marker_key''',
 	]
 
@@ -49,6 +107,7 @@ cmds = [
 fieldOrder = [
 	Gatherer.AUTO,
 	'mouseMarkerKey', 'otherMarkerKey', 'otherSymbol', 'otherOrganism',
+	'sequenceNum',
 	]
 
 # prefix for the filename of the output file
