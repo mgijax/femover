1523ac4d4a2c7a9f54151e428dfa2ab0ae05d869
Jonathan Beal <Jon.Beal@jax.org>
2012-03-28 12:08:38 +0000

    added new expression_result_sequence_num table

--- gather/expression_result_summary_gatherer.py
+++ gather/expression_result_summary_gatherer.py
@@ -6,6 +6,9 @@
 import Gatherer
 import logger
 import ADVocab
+import symbolsort
+import ReferenceCitations
+import types
 
 ###--- Functions ---###
 
@@ -69,10 +72,23 @@ class ExpressionResultSummaryGatherer (Gatherer.MultiFileGatherer):
 		symbolCol = Gatherer.columnNumber (cols, 'symbol')
 		keyCol = Gatherer.columnNumber (cols, '_Marker_key')
 
+		allSymbols = []
+
 		for row in rows:
 			symbol[row[keyCol]] = row[symbolCol]
+			allSymbols.append (row[symbolCol])
 
 		logger.debug ('Got %d marker symbols' % len(symbol))
+
+		allSymbols.sort (symbolsort.nomenCompare)
+
+		self.symbolSequenceNum = {}
+		i = 0
+		for sym in allSymbols:
+			i = i + 1
+			self.symbolSequenceNum[sym] = i
+
+		logger.debug ('Sorted %d marker symbols' % i) 
 		return symbol
 
 	def getDisplayableImagePanes(self):
@@ -155,17 +171,21 @@ class ExpressionResultSummaryGatherer (Gatherer.MultiFileGatherer):
 		strengthCol = Gatherer.columnNumber (cols, 'strength')
 		resultCol = Gatherer.columnNumber (cols, '_Result_key')
 		structureCol = Gatherer.columnNumber (cols,'_Structure_key')
+		ageMinCol = Gatherer.columnNumber (cols, 'ageMin')
+		ageMaxCol = Gatherer.columnNumber (cols, 'ageMax')
 
 		for row in rows:
 		    assayKey = row[assayCol]
 		    if extras.has_key(assayKey):
 	 		extras[assayKey].append( [ row[genotypeCol],
 				row[ageCol], row[strengthCol], row[resultCol],
-				row[structureCol] ] )
+				row[structureCol], row[ageMinCol],
+				row[ageMaxCol] ] )
 		    else:
 	 		extras[assayKey] = [ [ row[genotypeCol],
 				row[ageCol], row[strengthCol], row[resultCol],
-				row[structureCol] ] ]
+				row[structureCol], row[ageMinCol],
+				row[ageMaxCol] ] ]
 
 
 		logger.debug ('Got extra data for %d in situ assays' % \
@@ -184,21 +204,86 @@ class ExpressionResultSummaryGatherer (Gatherer.MultiFileGatherer):
 		ageCol = Gatherer.columnNumber (cols, 'age')
 		strengthCol = Gatherer.columnNumber (cols, 'strength')
 		structureCol = Gatherer.columnNumber (cols,'_Structure_key')
+		ageMinCol = Gatherer.columnNumber (cols, 'ageMin')
+		ageMaxCol = Gatherer.columnNumber (cols, 'ageMax')
 
 		for row in rows:
 		    assayKey = row[assayCol]
 		    if extras.has_key(assayKey):
 			extras[row[assayCol]].append( [ row[genotypeCol],
 				row[ageCol], row[strengthCol],
-				row[structureCol] ] )
+				row[structureCol], row[ageMinCol],
+				row[ageMaxCol] ] )
 		    else:
 			extras[row[assayCol]] = [ [ row[genotypeCol],
 				row[ageCol], row[strengthCol],
-				row[structureCol] ] ]
+				row[structureCol], row[ageMinCol],
+				row[ageMaxCol] ] ]
 
 		logger.debug ('Got extra data for %d gel assays' % \
 			len(extras))
 		return extras
+
+	def sortGenotypeData(self):
+		cols, rows = self.results[9]
+
+		genotypeCol = Gatherer.columnNumber (cols, '_Genotype_key')
+		allele1Col = Gatherer.columnNumber (cols, 'symbol1')
+		allele2Col = Gatherer.columnNumber (cols, 'symbol2')
+
+		byGenotype = {}
+
+		for row in rows:
+			genotypeKey = row[genotypeCol]
+			allele1 = row[allele1Col]
+			allele2 = row[allele2Col]
+
+			if not byGenotype.has_key(genotypeKey):
+				byGenotype[genotypeKey] = [ allele1, allele2 ]
+			else:
+				byGenotype[genotypeKey].append (allele1)
+				byGenotype[genotypeKey].append (allele2)
+
+		toSort = []
+		for (key, alleles) in byGenotype.items():
+			toSort.append ( (alleles, key) )
+
+		def custom (a, b):
+			# comparator to sort based on strings of allele
+			# symbols, such that a[i] is compared to b[i] for each
+			# value of i up to minimum length of a,b
+
+			max_i = min(len(a[0]), len(b[0]))
+
+			i = 0
+			while i < max_i:
+				if a[0][i] == None:
+					return -1
+				elif b[0][i] == None:
+					return 1
+
+				c = symbolsort.nomenCompare (a[0][i], b[0][i])
+				if c != 0:
+					return c
+				i = i + 1
+
+			if len(a[0]) > len(b[0]):
+				return 1
+			elif len(b[0]) > len(a[0]):
+				return -1
+			return 0
+
+		toSort.sort(custom)
+
+		self.byGenotype = {}
+		i = 0
+
+		for (alleles, key) in toSort:
+			i = i + 1
+			self.byGenotype[key] = i
+
+		logger.debug ('Sorted %d genotypes by allele pairs' % i)
+		return
 
 	def collateResults (self):
 
@@ -230,6 +315,8 @@ class ExpressionResultSummaryGatherer (Gatherer.MultiFileGatherer):
 			'sequence_num' ]
 		ertiRows = []
 
+		ageMinMax = {}		# result key -> (age min, age max)
+
 		# now, we need to walk through our assays to populate the list
 		# of data for each table
 
@@ -250,8 +337,8 @@ class ExpressionResultSummaryGatherer (Gatherer.MultiFileGatherer):
 			panes = []	# image panes for this result
 
 			if isGel:
-			    [ genotypeKey, age, strength, structureKey ] = \
-				items
+			    [ genotypeKey, age, strength, structureKey,
+				ageMin, ageMax ] = items
 
 			    if imagepaneKey:
 				panes = [ imagepaneKey ]
@@ -260,7 +347,7 @@ class ExpressionResultSummaryGatherer (Gatherer.MultiFileGatherer):
 				    hasImage = 1
 			else:
 			    [ genotypeKey, age, strength, resultKey,
-				structureKey ] = items
+				structureKey, ageMin, ageMax ] = items
 
 			    if panesForInSituResults.has_key(resultKey):
 				panes = panesForInSituResults[resultKey]
@@ -294,41 +381,204 @@ class ExpressionResultSummaryGatherer (Gatherer.MultiFileGatherer):
 
 			ersRows.append (outRow)
 
+			ageMinMax[newKey] = (ageMin, ageMax)
+
 			for paneKey in panes:
 			    paneRow = [ newKey, paneKey, len(ertiRows) + 1 ]
 			    ertiRows.append (paneRow)
-
-#		    hasImage = 0	# is there a displayable image for this
-#		    			# ...result?
-#		    panes = []		# panes to associate with this result
-#
-#		    if isGel:
-#			[ genotypeKey, age, strength, structureKey ] = \
-#				gelExtras[assayKey]
-#
-#			if displayablePanes.has_key(imagepaneKey):
-#				hasImage = 1
-#
-#			if imagepaneKey:
-#				panes = [ imagepaneKey ]
-#
-#		    else:
-#			[ genotypeKey, age, strength, resultKey,
-#				structureKey ] = inSituExtras[assayKey]
-#
-#			if panesForInSituResults.has_key(resultKey):
-#			    panes = panesForInSituResults[resultKey]
-#
-#			    for paneKey in panes:
-#				if displayablePanes.has_key(paneKey):
-#				    hasImage = 1
 
 		logger.debug ('Got %d GXD result summary rows' % len(ersRows))
 		logger.debug ('Got %d GXD result/pane rows' % len(ertiRows))
 
 		self.output.append ( (ersCols, ersRows) )
 		self.output.append ( (ertiCols, ertiRows) )
+
+		(cols, rows) = self.getSequenceNumTable (ersCols, ersRows,
+			ageMinMax)
+
+		self.output.append ( (cols, rows) )
 		return
+
+	def getSymbolSequenceNum (self, symbol):
+		if self.symbolSequenceNum.has_key(symbol):
+			return self.symbolSequenceNum[symbol]
+		return len(self.symbolSequenceNum) + 1
+
+	def getAssayTypeSequenceNum (self, assayType):
+		if assayType == 'Immunohistochemistry':
+			return 1
+		elif assayType == 'RNA in situ':
+			return 2
+		elif assayType == 'In situ reporter (knock in)':
+			return 3
+		elif assayType == 'Northern blot':
+			return 4
+		elif assayType == 'Western blot':
+			return 5
+		elif assayType == 'RT-PCR':
+			return 6
+		elif assayType == 'RNase protection':
+			return 7
+		elif assayType == 'Nuclease S1':
+			return 8
+		return 9
+
+	def getStageSequenceNum (self, stage):
+		return int(stage)
+
+	def getExpressedSequenceNum (self, isExpressed):
+		if isExpressed == 'Yes':
+			return 1
+		elif isExpressed == 'No':
+			return 2
+		return 3
+
+	def getGenotypeSequenceNum (self, genotypeKey):
+		if self.byGenotype.has_key(genotypeKey):
+			return self.byGenotype[genotypeKey]
+		return len(self.byGenotype) + 1
+
+	def getSequenceNumTable (self, ersCols, ersRows, ageMinMax):
+		# build and return the rows (and columns) for the
+		# expression_result_sequence_num table
+
+		self.sortGenotypeData()
+
+		cols = [ 'result_key', 'by_assay_type', 'by_gene_symbol',
+			'by_anatomical_system', 'by_age', 'by_strucure',
+			'by_expressed', 'by_mutant_alleles', 'by_reference' ]
+		rows = []
+
+		resultKeys = []		# all result_key values
+
+		# We need to generate one row for each incoming row from 
+		# 'ersRows'.  Each of these rows will have multiple pre-
+		# computed sorts (as in 'cols'), and each of those needs to be
+		# pre-computed as a multi-level sort.
+
+		# Each of these ordering lists is used to pre-compute one of
+		# the sorts.  Each list will be populated by tuples containing
+		# the values to be considered in the sort for that item, and
+		# the tuple's last entry will be the result_key.
+
+		byAssayType = []
+		bySymbol = []
+		bySystem = []
+		byAge = []
+		byStructure = []
+		byExpressed = []
+		byMutants = []
+		byReference = []
+
+		# fill in sortable data for each of the ordering lists
+		# (right now we only do byStructure)
+
+		resultKeyCol = Gatherer.columnNumber (ersCols, 'result_key')
+		assayKeyCol = Gatherer.columnNumber (ersCols, '_Assay_key')
+		assayTypeCol = Gatherer.columnNumber (ersCols, 'assayType')
+		symbolCol = Gatherer.columnNumber (ersCols, 'symbol')
+		systemCol = Gatherer.columnNumber (ersCols, 'system')
+		stageCol = Gatherer.columnNumber (ersCols, 'stage')
+		expressedCol = Gatherer.columnNumber (ersCols, 'isExpressed')
+		refsKeyCol = Gatherer.columnNumber (ersCols, '_Refs_key')
+		structureKeyCol = Gatherer.columnNumber (ersCols,
+			'structureKey')
+		genotypeKeyCol = Gatherer.columnNumber (ersCols,
+			'_Genotype_key')
+
+		logger.debug ('identified columns for sorts')
+
+		for row in ersRows:
+		    resultKey = row[resultKeyCol]
+
+		    structure = ADVocab.getSequenceNum (row[structureKeyCol])
+		    stage = self.getStageSequenceNum (row[stageCol])
+		    ageMin = ageMinMax[resultKey][0]
+		    ageMax = ageMinMax[resultKey][1]
+		    expressed = self.getExpressedSequenceNum (row[expressedCol])
+		    symbol = self.getSymbolSequenceNum (row[symbolCol])
+		    assay = self.getAssayTypeSequenceNum (row[assayTypeCol])
+		    system = row[systemCol].lower()
+		    mutants = self.getGenotypeSequenceNum (row[genotypeKeyCol])
+		    refs = ReferenceCitations.getSequenceNum (row[refsKeyCol])
+
+		    # add records to the various individual ordering lists
+
+		    byAssayType.append ( (assay, symbol, ageMin, ageMax,
+			    structure, stage, expressed, resultKey) )
+		    bySymbol.append ( (symbol, assay, ageMin, ageMax,
+			    structure, stage, expressed, resultKey) )
+		    bySystem.append ( (system, structure, stage, ageMin,
+			    ageMax, expressed, symbol, resultKey) )
+		    byAge.append ( (ageMin, ageMax, structure, stage, 
+			    expressed, symbol, assay, resultKey) )
+		    byStructure.append ( (structure, stage, ageMin, ageMax,
+			    expressed, symbol, assay, resultKey) )
+		    byExpressed.append ( (expressed, symbol, assay, ageMin,
+			    ageMax, structure, stage, resultKey) )
+		    byMutants.append ( (mutants, symbol, assay, ageMin,
+			    ageMax, structure, stage, resultKey) )
+		    byReference.append ( (refs, symbol, assay, ageMin, ageMax,
+			    structure, stage, resultKey) )
+
+		    resultKeys.append (resultKey)
+
+		logger.debug ('compiled ordering lists')
+
+		# sort the individual ordering lists
+
+		for orderingList in [ byAssayType, bySymbol, bySystem, byAge,
+			byStructure, byExpressed, byMutants, byReference]:
+				orderingList.sort()
+
+		logger.debug ('sorted ordering lists')
+
+		# produce the individual output row for each result key
+
+		output = {}		# result key -> [ sort 1, ... n ]
+
+		for key in resultKeys:
+			output[key] = [ key ]
+
+		for orderingList in [ byAssayType, bySymbol, bySystem, byAge,
+			byStructure, byExpressed, byMutants, byReference ]:
+				i = 0
+
+				# remember which result keys were not seen for
+				# this orderingList so far
+
+				notDone = {}
+				for key in resultKeys:
+					notDone[key] = 1
+
+				# order those we know about in 'orderingList'
+
+				for row in orderingList:
+					i = i + 1
+					output[row[-1]].append(i)
+					del notDone[row[-1]]
+
+				# order those left over in 'notDone'
+
+				keys = notDone.keys()
+				keys.sort()
+
+				for key in keys:
+					i = i + 1
+					output[row[-1]].append(i)
+
+		logger.debug ('collated ordering lists into dict')
+
+		# extract the individual rows and bundle them into 'rows'
+
+		keys = output.keys()
+		keys.sort()
+
+		for key in keys:
+			rows.append (output[key])
+
+		logger.debug ('produced %d ordering rows' % len(rows))
+		return cols, rows
 
 ###--- globals ---###
 
@@ -403,7 +653,9 @@ cmds = [
 		r._Strength_key,
 		st.strength,
 		r._Result_key,
-		rs._Structure_key
+		rs._Structure_key,
+		s.ageMin,
+		s.ageMax
 	from GXD_Specimen s,
 		GXD_InSituResult r,
 		GXD_Strength st,
@@ -420,7 +672,9 @@ cmds = [
 		g.age,
 		st._Strength_key,
 		st.strength,
-		gs._Structure_key
+		gs._Structure_key,
+		g.ageMin,
+		g.ageMax
 	from GXD_GelLane g,
 		GXD_GelBand b,
 		GXD_Strength st,
@@ -429,6 +683,19 @@ cmds = [
 		and g._GelLane_key = b._GelLane_key
 		and b._Strength_key = st._Strength_key
 		and g._GelLane_key = gs._GelLane_key''',
+
+	# 9. allele pairs for genotypes cited in GXD data
+	'''select gap._Genotype_key,
+		a1.symbol as symbol1,
+		a2.symbol as symbol2
+	from GXD_AllelePair gap
+	inner join ALL_Allele a1 on (gap._Allele_key_1 = a1._Allele_key)
+	left outer join ALL_Allele a2 on (gap._Allele_key_2 = a2._Allele_key)
+	where exists (select 1 from GXD_Specimen s
+			where s._Genotype_key = gap._Genotype_key)
+		or exists (select 1 from GXD_GelLane g
+			where g._Genotype_key = gap._Genotype_key)
+	order by gap.sequenceNum''',
 	]
 
 files = [
@@ -444,6 +711,12 @@ files = [
 		[ Gatherer.AUTO, 'result_key', '_ImagePane_key',
 		'sequence_num' ],
 		'expression_result_to_imagepane'),
+
+	('expression_result_sequence_num',
+		[ 'result_key', 'by_assay_type', 'by_gene_symbol',
+		'by_anatomical_system', 'by_age', 'by_strucure',
+		'by_expressed', 'by_mutant_alleles', 'by_reference' ],
+		'expression_result_sequence_num'),
 	]
 
 # global instance of a ExpressionResultSummaryGatherer
