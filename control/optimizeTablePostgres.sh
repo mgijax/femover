148c1a86aeca9fdd4d9d8e7b50215f28ce05ed86
Jonathan Beal <Jon.Beal@jax.org>
2012-05-03 19:20:53 +0000

    initial addition

---
+++ control/optimizeTablePostgres.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# run 'vacuum' and 'analyze' on a Postgres table to optimize its size and
+# update its data statistics (needed for the query planner to make good
+# choices)
+
+# Usage: optimizeTablePostgres.sh <server> <database> <user> <password> <table name>
+
+. ../Configuration
+
+PGPASSWORD=$4
+export PGPASSWORD
+
+psql -h $1 -d $2 -U $3 --command "vacuum analyze $5;" > ${LOG_DIR}/$5.optimize.log 2>&1
+exit $?
