aebc0efe6376dbd34368e1b0bc0c94dc7f30b96a
MGI Admin <mgiadmin@jax.org>
2013-02-15 13:55:29 +0000

    replacement for dynamic data google doc

---
+++ gather/test_stats_gatherer.py
@@ -0,0 +1,240 @@
+#!/usr/local/bin/python
+# 
+# Gathers data for the 'test_stats' table in the front-end database.
+# Reads multiple user-defined queries designed to generate test data
+# for comparison on the front-end, using automated tests.
+#
+#	Author: kstone 2013/02/14
+#
+
+import Gatherer
+import logger
+import dbAgnostic
+import re
+
+SQL_VAR_PATTERN = "\$\{([a-zA-Z0-9]+)\}"
+# compile the regex for parsing out variable strings in the SQL statements
+sql_re = re.compile(SQL_VAR_PATTERN)
+
+#column definition
+GROUP = "group" 
+ID = "id"
+DESCRIPTION = "description"
+SQLSTATEMENT = "sqlstatement" 
+RETURNDATA = "returndata"
+ERROR = "error"
+UPDATED = "updated"
+coldefs = {'1':GROUP,'2':ID,'3':DESCRIPTION,'4':SQLSTATEMENT,'5':RETURNDATA,'6':ERROR}
+
+
+test_sqls = [
+"Assay Types",
+{	
+	ID:"RNaseProtectionResultCount", 
+	DESCRIPTION:"Assay Result count for RNaseProtection assay type", 
+	SQLSTATEMENT:"""
+	select count(*) from gxd_expression ge, gxd_assaytype gt where ge._assaytype_key=gt._assaytype_key and ge.isforgxd=1 and gt.assaytype='RNase protection'
+	""" },
+{	
+	ID:"WesternBlotResultCount", 
+	DESCRIPTION:"Assay Result count for Western Blot assay type", 
+	SQLSTATEMENT:"""
+	select count(*) from gxd_expression ge, gxd_assaytype gt where ge._assaytype_key=gt._assaytype_key and ge.isforgxd=1 and gt.assaytype='Western blot'
+	""" },
+{	
+	ID:"NorthernBlotResultCount", 
+	DESCRIPTION:"Assay Result count for Northern Blot assay type", 
+	SQLSTATEMENT:"""
+	select count(*) from gxd_expression ge, gxd_assaytype gt where ge._assaytype_key=gt._assaytype_key and ge.isforgxd=1 and gt.assaytype='Northern blot'
+	""" },
+]
+
+rows_by_id = {}
+
+CIRCULAR_ERROR = "###CircularError####"
+REFERENCE_ERROR = "###ReferenceError####"
+EXISTING_ERROR = "###ExistingError###"
+
+###--- Functions to be moved into a lib module ---###
+def iterateSqls():
+	group = test_sqls[0]
+	
+	all_rows = []
+	for test_sql in test_sqls[1:]:
+		id = test_sql[ID]
+		# sanitise input
+		test_sql[DESCRIPTION] = test_sql[DESCRIPTION].strip()
+		test_sql[SQLSTATEMENT] = test_sql[SQLSTATEMENT].strip()
+		test_sql[UPDATED] = False
+		test_sql[ERROR] = ""
+		test_sql[RETURNDATA] = ""
+		test_sql[GROUP] = group
+		rows_by_id[id] = test_sql
+		all_rows.append(test_sql)
+	for row in all_rows:
+		run_sql_statement(row)
+	#logger.debug(all_rows)
+	return all_rows
+	
+	
+
+# process SQL and perform any variable substitutions
+def run_sql_statement(row,idStack=[]):
+	global sql_re
+	global rows_by_id
+
+	if row[UPDATED]:
+		if row[ERROR]:
+			return (EXISTING_ERROR,"Referenced id %s has an error"%row[ID])
+		return row[RETURNDATA]
+
+	if row[ID] in idStack:
+		error_text = "Circular Variable Path: %s references %s"%(idStack[-1],row[ID]) 
+		return (CIRCULAR_ERROR,error_text) 
+
+	idStack.append(row[ID])
+
+	sqlstatement = row[SQLSTATEMENT]
+
+	# find out if there are any variable substitutions that need to be made
+	vars = sql_re.findall(sqlstatement)
+	#print "processing vars: %s in %s"%(vars,row[ID].cell.text)
+	if vars:
+		for var in vars:
+			if var not in rows_by_id:
+				error_text = "Reference ID %s does not exist"%var
+				return (REFERENCE_ERROR,error_text)
+			return_value = run_sql_statement(rows_by_id[var],idStack)
+			if CIRCULAR_ERROR in return_value:
+				error_text = return_value[1]
+				# there was an error. Clear the return data cell
+				row[RETURNDATA] = ''
+				row[ERROR] = error_text
+				row[UPDATED] = True
+				return return_value
+			elif REFERENCE_ERROR in return_value:
+				error_text = return_value[1]
+				# there was an error. Clear the return data cell
+				row[RETURNDATA] = ''
+				row[ERROR] = error_text
+				row[UPDATED] = True
+				return return_value
+			elif EXISTING_ERROR in return_value:
+				error_text = return_value[1]
+				# there was an error. Clear the return data cell
+				row[RETURNDATA] = ''
+				row[ERROR] = error_text
+				row[UPDATED] = True
+				return return_value
+			else:
+				# replace the variable in sqlstatement with return data
+				pattern = "\$\{%s\}"%var
+				#print "replacing with pattern %s"%pattern
+				sqlstatement = re.sub(pattern,parse_sql_data(return_value),sqlstatement)
+
+	logger.debug("running sql %s"%sqlstatement)
+	data = ''
+	try:
+		(dbcols,dbrows) = dbAgnostic.execute(sqlstatement.strip())
+		data = parse_sql_data(dbrows)
+		logger.debug("ran SQL for %s. Got value %s"%(row[ID],data))
+		row[RETURNDATA] = data
+	except Exception, e:
+		message = e[1]
+		logger.debug(message)
+		row[ERROR] = "SQL error: %s"%message
+		#print "ran SQL for %s. Got error"%(row[ID].cell.text)
+	row[UPDATED] = True
+	return data
+
+# do additional/special formatting of data before it gets written back to google
+def parse_sql_data(inData):
+	if len(inData) > 0:
+		rowData = []
+		for row in inData:
+			if len(row) > 1:
+				# we have multiple columns in result set, need to join them	
+				rowData.append("ROW[%s]"%",".join(["%s"%x for x in row]))
+			else:
+				rowData.append("%s"%row[0])
+		outData = ",".join(rowData)
+	else:
+		outData = inData
+	
+	return outData
+
+
+###--- Classes ---###
+
+class TestStatsGatherer (Gatherer.Gatherer):
+	# Is: a data gatherer for the test_stats table
+
+	def collateResults(self):
+		cols = [ '_test_stats', 'id', 'group_type', 'description', 'sql_statement','test_data','error']
+		data_rows = iterateSqls()
+
+		count = 0
+		dbrows = []	
+		for data in data_rows:
+			count += 1
+			dbrows.append( [count,data[ID],data[GROUP],data[DESCRIPTION],data[SQLSTATEMENT],data[RETURNDATA],data[ERROR]])
+			
+			"""
+			self.addColumn("test_stats_key",count,row,cols)
+			self.addColumn("id",data[ID],row,cols)
+			self.addColumn("group_type",data[GROUP],row,cols)
+			self.addColumn("description",data[DESCRIPTION],row,cols)
+			self.addColumn("sql_statement",data[SQLSTATEMENT],row,cols)
+			self.addColumn("test_data",data[RETURNDATA],row,cols)
+			self.addColumn("error",data[ERROR],row,cols)
+			"""
+		self.finalColumns = cols
+		self.finalResults = dbrows
+		"""
+	
+
+		self.addColumn ('_Probe_key', probeKey, row, cols)
+		self.addColumn ('probe_name', probeName, row, cols)
+		self.addColumn ('probe_preparation', probePrep, row,
+			cols)
+		self.addColumn ('visualized_with', visualizedWith,
+			row, cols)
+		self.addColumn ('is_direct_detection',
+			isDirectDetection, row, cols)
+		self.addColumn ('note', note, row, cols)
+		self.addColumn ('detection_system', detectionSystem,
+			row, cols)
+		self.addColumn ('_Antibody_key', antibodyKey, row, cols)
+		self.addColumn ('antibody', antibodyName, row, cols)
+		self.addColumn ('assay_type', assayType, row, cols)
+		self.addColumn ('reporter_gene', reporter, row, cols)
+		self.addColumn ('has_image', image, row, cols)
+		"""
+		return
+
+	def postprocessResults(self):
+		return
+
+###--- globals ---###
+
+# The gatherer class becomes sad when there are no queries to execute at creation time
+# This query won't actually be used by this code
+cmds = ["""select 1 from mrk_marker limit 1"""] 
+
+# order of fields (from the query results) to be written to the
+# output file
+fieldOrder = [ '_test_stats', 'id', 'group_type', 'description', 'sql_statement', 
+        'test_data', 'error' ]
+
+# prefix for the filename of the output file
+filenamePrefix = 'test_stats'
+
+# global instance of a AssayGatherer
+gatherer = TestStatsGatherer (filenamePrefix, fieldOrder, cmds)
+
+###--- main program ---###
+
+# if invoked as a script, use the standard main() program for gatherers and
+# pass in our particular gatherer
+if __name__ == '__main__':
+	Gatherer.main (gatherer)
