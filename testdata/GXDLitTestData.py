acb9ec699947b73e1a708d6c5fa3bd4ca44d47cd
MGI Admin <mgiadmin@jax.org>
2013-02-15 18:38:09 +0000

    new tests

---
+++ testdata/GXDLitTestData.py
@@ -0,0 +1,64 @@
+#!/usr/local/bin/python
+
+# this TestData import brings in the constants used below (E.g. ID, DESCRIPTION,...)
+from TestData import *
+
+# The list of queries to generate GXD Lit test data
+Queries = [
+###--- Miscellaneous GXD Lit tests
+{	ID:"gxdLitPax1Count",
+	DESCRIPTION:"Count of gxdlit records for Pax1",
+	SQLSTATEMENT:"""
+	select count(distinct _index_key) from gxd_index i, mrk_marker m where m._marker_key=i._marker_key and m.symbol='Pax1';
+	"""
+},
+{	ID:"gxdlitTS9Count",
+	DESCRIPTION:"Count of gxdlit records for TS9",
+	SQLSTATEMENT:"""
+	select count(distinct _index_key) from gxd_index_stages ins where ins._stageid_key in (74741,74742,74743);
+	"""
+},
+{	ID:"gxdlitAge2Count",
+	DESCRIPTION:"Count of gxdlit records for Age 2",
+	SQLSTATEMENT:"""
+	select count(distinct _index_key) from gxd_index_stages ins where ins._stageid_key =74732;
+	"""
+},
+{	ID:"gxdlitImmunohistochemistryCount",
+	DESCRIPTION:"Count of gxdlit records for Immunohistochemistry",
+	SQLSTATEMENT:"""
+	select count(distinct _index_key) from gxd_index_stages ins where ins._indexassay_key in (74717,74719);
+	"""
+},
+{	ID:"gxdlistPostnatalCount",
+	DESCRIPTION:"Count of gxdlit records for Postnatal",
+	SQLSTATEMENT:"""
+	select count(distinct _index_key) from gxd_index_stages ins 
+		where ins._stageid_key =74770;
+	"""
+},
+{	ID:"gxdlitEmbryonicCount",
+	DESCRIPTION:"Count of gxdlit records for Embryonic",
+	SQLSTATEMENT:"""
+	select count(distinct _index_key) from gxd_index_stages ins where ins._stageid_key !=74770;
+	"""
+},
+{	ID:"gxdlitRNAInSituCount",
+	DESCRIPTION:"Count of gxdlit records for RNA in situ",
+	SQLSTATEMENT:"""
+	select count(distinct _index_key) from gxd_index_stages ins 
+		where ins._indexassay_key in (74718,74720);
+	"""
+},
+{	ID:"gxdlitCombinedCount",
+	DESCRIPTION:"Count of gxdlit records for a combination of parameters (marker, assay type, stage)",
+	SQLSTATEMENT:"""
+	select count(distinct gi._index_key) from gxd_index gi, gxd_index_stages ins, mrk_marker m 
+		where gi._index_key=ins._index_key and m._marker_key=gi._marker_key 
+			and m.symbol='Pax2' 
+			and ins._indexassay_key=74722 
+			and ins._stageid_key=74770;
+	"""
+},
+]
+
--- testdata/GXDTestData.py
+++ testdata/GXDTestData.py
@@ -55,6 +55,56 @@ Queries = [
 	"""
 },
 
-###--- Assay type tests
+###--- Theiler Stage Tests
+{	ID:"ts6ResultCount",
+	DESCRIPTION:"Assay Result count for Theiler Stage 6",
+	SQLSTATEMENT:"""
+	select count(*) from gxd_expression ge, gxd_Structure gs where ge._structure_key=gs._structure_key and ge.isforgxd=1 and gs._stage_key=6
+	"""
+},
+{	ID:"allResults",
+	DESCRIPTION:"count of all gxd results",
+	SQLSTATEMENT:"""
+	select count(*) from gxd_expression ge, gxd_Structure gs where ge._structure_key=gs._structure_key and ge.isforgxd=1
+	"""
+},
+{	ID:"ts6or7ResultCount",
+	DESCRIPTION:"Assay Result count for TS 6 or 7",
+	SQLSTATEMENT:"""
+	select count(*) from gxd_expression ge, gxd_Structure gs where ge._structure_key=gs._structure_key and ge.isforgxd=1 and gs._stage_key in (6,7)
+	"""
+},
+{	ID:"ts28ResultCount",
+	DESCRIPTION:"Assay Result count for TS 28",
+	SQLSTATEMENT:"""
+	select count(*) from gxd_expression ge, gxd_Structure gs where ge._structure_key=gs._structure_key and ge.isforgxd=1 and gs._stage_key=28
+	"""
+},
+{	ID:"allGenes",
+	DESCRIPTION:"Count of all GXD markers",
+	SQLSTATEMENT:"""
+	select count(distinct _marker_key) from gxd_expression ge, gxd_Structure gs where ge._structure_key=gs._structure_key and ge.isforgxd=1
+	"""
+},
+
+###--- Age Tests
+{	ID:"age2-3ResultCount",
+	DESCRIPTION:"Assay Result count for ages 2.0,2.5,3.0",
+	SQLSTATEMENT:"""
+	select count(*) from gxd_Expression where isforgxd=1 and (agemin <3.25 and agemax >=1.75)
+	"""
+},
+{	ID:"age5and6and7ResultCount",
+	DESCRIPTION:"Assay Result count for ages 5.0,6.0,7.0",
+	SQLSTATEMENT:"""
+	select count(*) from gxd_Expression where isforgxd=1 and ((agemin <5.25 and agemax >=4.75) or (agemin <6.25 and agemax >=5.75) or (agemin <7.25 and agemax >=6.75))
+	"""
+},
+{	ID:"age1and4ResultCount",
+	DESCRIPTION:"Result count for ages 1.0 and 4.0",
+	SQLSTATEMENT:"""
+	select count(*) from gxd_Expression where isforgxd=1 and ((agemin <1.25 and agemax >=0.75) or (agemin <4.25 and agemax >=3.75))
+	"""
+},
 ]
 
