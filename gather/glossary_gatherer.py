ed452f62dd1a03ef2c9ff4531e9f92e0dd0c0842
MGI Admin <mgiadmin@jax.org>
2013-07-10 17:46:15 +0000

    adding glossary index

---
+++ gather/glossary_gatherer.py
@@ -0,0 +1,66 @@
+#!/usr/local/bin/python
+# 
+# 	Gathers data for the glossary index and term pages
+#	Reads the glossary.rcd file from the data directory
+#
+#	Author: kstone 2013/07/10
+#
+
+import Gatherer
+import logger
+import dbAgnostic
+import rcdlib
+import config
+
+###--- Classes ---###
+class GlossaryGatherer (Gatherer.Gatherer):
+	# Is: a data gatherer for the glossary_term table
+	def readRcd(self):
+		filepath = config.GLOSSARY_FILE
+		try:
+                        rcdFile = rcdlib.RcdFile (filepath, rcdlib.Rcd, 'key')
+                except rcdlib.error, message:
+                        raise error, message
+		return rcdFile
+
+	def collateResults(self):
+		cols = ['unique_key','glossary_key','display_name','definition']
+		rcdFile = self.readRcd()
+
+		count = 0
+		dbrows = []	
+		count = 0
+		for key,rcd in rcdFile.items():
+			count += 1
+			dbrows.append((count,key,rcd["displayName"],rcd["definition"]))
+			
+		self.finalColumns = cols
+		self.finalResults = dbrows
+		return
+
+	def postprocessResults(self):
+		return
+
+###--- globals ---###
+
+# The gatherer class becomes sad when there are no queries to execute at creation time
+# This query won't actually be used by this code
+# TODO: please modify if you know how to fix this without the need for my hack
+cmds = ["""select 1 from mrk_marker limit 1"""] 
+
+# order of fields (from the query results) to be written to the
+# output file
+fieldOrder = ['unique_key','glossary_key','display_name','definition']
+
+# prefix for the filename of the output file
+filenamePrefix = 'glossary_term'
+
+# global instance of a AssayGatherer
+gatherer = GlossaryGatherer (filenamePrefix, fieldOrder, cmds)
+
+###--- main program ---###
+
+# if invoked as a script, use the standard main() program for gatherers and
+# pass in our particular gatherer
+if __name__ == '__main__':
+	Gatherer.main (gatherer)
---
+++ schema/glossary_term.py
@@ -0,0 +1,53 @@
+#!/usr/local/bin/python
+
+import Table
+
+# Note: All table and field names should be all-lowercase with underscores
+# used to separate words.
+
+###--- Globals ---###
+
+# name of this database table
+tableName = 'glossary_term'
+
+# PgSQL statement to create this table
+createStatement = '''CREATE TABLE %s  ( 
+	unique_key		int		not null,
+	glossary_key			text		not null,
+	display_name			text		not null,
+	definition		text		not null,
+	PRIMARY KEY(unique_key))''' % tableName
+
+# Maps from index suffix to create statement for that index.  In each
+# statement, the first %s is for the index name, and the second is for the
+# table name.
+indexes = {
+	'glossary_key' : 'create index %s on %s (glossary_key)',
+	}
+keys = {}
+
+
+clusteredIndex=None
+# comments describing the table, columns, and indexes
+comments = {
+	Table.TABLE : 'contains data from the glossary.rcd file',
+	Table.COLUMN : {
+		'unique_key' : 'unique identifier for this table',
+		'glossary_key' : 'readable key for linking',
+		'display_name' : 'glossary term display value',
+		'definition' : 'glossary term definition',
+		},
+	Table.INDEX : {
+		},
+	}
+
+# global instance of this Table object
+table = Table.Table (tableName, createStatement, indexes, keys, comments,
+		clusteredIndex)
+
+###--- Main program ---###
+
+# if executed as a script, pass the global Table object into the general
+# main program for Table subclasses
+if __name__ == '__main__':
+	Table.main(table)
