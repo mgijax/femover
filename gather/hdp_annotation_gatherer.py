f64eaf6af92604ee468c12fdddc2f41555a99544
MGI Admin <mgiadmin@jax.org>
2013-07-23 17:31:13 +0000

    TR11423/hdp

--- gather/hdp_annotation_gatherer.py
+++ gather/hdp_annotation_gatherer.py
@@ -20,36 +20,48 @@ HDPAnnotationGatherer = Gatherer.Gatherer
 
 cmds = [
 
-	# mouse/genotypes annotated to OMIM terms (1005)
-	# human/genes annotated to OMIM terms (1006)
-	# make human/genotype key = null
-	'''
-	select distinct gg._Marker_key, v._Term_key, v._AnnotType_key, v._Object_key, t.term, a.accID, vv.name
-	from VOC_Annot v , VOC_Term t, VOC_Vocab vv, GXD_AlleleGenotype gg, ACC_Accession a
-	where v._AnnotType_key in (1005)
-	and v._Term_key = t._Term_key
-	and v._Object_key = gg._Genotype_key
-	and v._Term_key = a._Object_key
-	and a._MGIType_key = 13
-	and a.private = 0
-	and a.preferred = 1
-	and t._Vocab_key = vv._Vocab_key
-	union
-	select distinct v._Object_key, v._Term_key, v._AnnotType_key, null::integer, t.term, a.accID, vv.name
-	from VOC_Annot v , VOC_Term t, VOC_Vocab vv, ACC_Accession a
-	where v._AnnotType_key in (1006)
-	and v._Term_key = t._Term_key
-	and v._Term_key = a._Object_key
-	and a._MGIType_key = 13
-	and a.private = 0
-	and a.preferred = 1
-	and t._Vocab_key = vv._Vocab_key
-	'''
+        # mouse/genotypes annotated to OMIM terms (1005)
+        # human/genes annotated to OMIM terms (1006)
+        # make human/genotype key = null
+        #
+        # exclude: NOT annotations genotypes
+        # exclude: 'Transgenic (Reporter)' alleles (847129)
+        # exclude: wild type alleles
+        '''
+        select distinct gg._Marker_key, gg._Allele_key, v._Term_key, v._AnnotType_key, 
+                v._Object_key, t.term, a.accID, vv.name
+        from VOC_Annot v, VOC_Term t, VOC_Vocab vv, 
+                GXD_AlleleGenotype gg, ALL_Allele ag, ACC_Accession a
+        where v._AnnotType_key in (1005)
+        and v._Qualifier_key != 1614157
+        and v._Term_key = t._Term_key
+        and v._Object_key = gg._Genotype_key
+        and gg._Allele_key = ag._Allele_key
+        and ag._Allele_Type_key != 847129
+        and ag.isWildType = 0
+        and v._Term_key = a._Object_key
+        and a._MGIType_key = 13
+        and a.private = 0
+        and a.preferred = 1
+        and t._Vocab_key = vv._Vocab_key
+        union
+        select distinct v._Object_key, v._Term_key, v._AnnotType_key, 
+                null::integer, null::integer, t.term, a.accID, vv.name
+        from VOC_Annot v , VOC_Term t, VOC_Vocab vv, ACC_Accession a
+        where v._AnnotType_key in (1006)
+        and v._Term_key = t._Term_key
+        and v._Term_key = a._Object_key
+        and a._MGIType_key = 13
+        and a.private = 0
+        and a.preferred = 1
+        and t._Vocab_key = vv._Vocab_key
+        ''',
+
 	]
 
 # order of fields (from the query results) to be written to the
 # output file
-fieldOrder = [ Gatherer.AUTO, '_Marker_key', '_Term_key', '_AnnotType_key', '_Object_key', 'term', 'accID', 'name' ]
+fieldOrder = [ Gatherer.AUTO, '_Marker_key', '_Allele_key', '_Term_key', '_AnnotType_key', '_Object_key', 'term', 'accID', 'name' ]
 
 # prefix for the filename of the output file
 filenamePrefix = 'hdp_annotation'
