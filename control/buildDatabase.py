c0d03141c0bc521754c14b143b4731cdde4019bd
Jonathan Beal <Jon.Beal@jax.org>
2014-02-26 20:51:22 +0000

    added marker_related_marker to set of marker gatherers

--- control/buildDatabase.py
+++ control/buildDatabase.py
@@ -187,7 +187,7 @@ MARKERS = [ 'marker', 'marker_id', 'marker_synonym', 'marker_to_allele',
 		'marker_to_probe', 'marker_count_sets', 'marker_alias',
 		'marker_biotype_conflict', 'marker_searchable_nomenclature',
 		'homology_cluster', 'marker_qtl_experiments',
-		'marker_microarray', 'marker_to_term',
+		'marker_microarray', 'marker_to_term', 'marker_related_marker',
 	]
 PROBES = [ 'probe', 'probe_clone_collection', 'probe_to_sequence',
 	]
