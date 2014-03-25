ee4fbe6d87f36c1c9c438e378faae1683851ede1
MGI Admin <mgiadmin@jax.org>
2014-03-25 16:46:25 +0000

    adding some marker queries

--- testdata/MarkerTestData.py
+++ testdata/MarkerTestData.py
@@ -12,6 +12,24 @@ Queries = [
 	select count(*) from mrk_marker where _organism_key in (1) and _marker_status_key!=2 ;
 	"""
 },
+{	ID:"markerCountMcv1",
+	DESCRIPTION:"Count of polymorphic pseudogene markers",
+	SQLSTATEMENT:"""
+	select count(distinct m._marker_key) 
+	from mrk_marker m join mrk_mcv_cache mcv on mcv._marker_key=m._marker_key 
+	where m._marker_status_key!=2 
+		and mcv._mcvterm_key=7288449 
+	"""
+},
+{	ID:"markerCountMcv2",
+	DESCRIPTION:"Count of polymorphic pseudogene markers",
+	SQLSTATEMENT:"""
+	select count(distinct m._marker_key) 
+	from mrk_marker m join mrk_mcv_cache mcv on mcv._marker_key=m._marker_key 
+	where m._marker_status_key!=2 
+		and mcv._mcvterm_key in (7288449,6238182,6238183) 
+	"""
+},
 # copy above lines to make more tests
 ]
 
