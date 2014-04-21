3b42c6699ea5cc5665bc3e5d877144b886a21876
MGI Admin <mgiadmin@jax.org>
2014-04-21 13:28:48 +0000

    changing unused snp column to imsr_count
    changing unused snp column to imsr count

--- schema/marker_counts.py
+++ schema/marker_counts.py
@@ -32,7 +32,7 @@ createStatement = '''CREATE TABLE %s  (
 	human_disease_count			int	NULL,
 	alleles_with_human_disease_count	int	NULL,
 	antibody_count				int	NULL,
-	snp_count				int	NULL,
+	imsr_count				int	NULL,
 	PRIMARY KEY(marker_key))''' % tableName
 
 # Maps from index suffix to create statement for that index.  In each
@@ -70,7 +70,7 @@ comments = {
 		'human_disease_count' : 'count of human diseases for which this marker has mouse models',
 		'alleles_with_human_disease_count' : 'count of alleles of this marker which are mouse models of a human disease',
 		'antibody_count' : 'count of antibodies',
-		'snp_count' : 'count of single-coordinate SNPs',
+		'imsr_count' : 'count of lines/mice in IMSR',
 		},
 	}
 
