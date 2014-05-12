f494110c8ed4d6983d9f05454638905d64fca3cd
Jonathan Beal <Jon.Beal@jax.org>
2014-05-12 11:34:30 +0000

    added forget() method to help memory usage

--- lib/python/KeyGenerator.py
+++ lib/python/KeyGenerator.py
@@ -2,6 +2,8 @@
 # Purpose: to provide an easy means for generating unique keys for tables,
 #	where those keys will need to be accessible to other tables
 
+import gc
+
 class KeyGenerator:
 	def __init__ (self, tableName = None):
 		# constructor
@@ -30,3 +32,17 @@ class KeyGenerator:
 		# Retrieve the table name for this KeyGenerator.
 
 		return self.tableName
+
+	def forget (self):
+		# In some applications, you may want to try to conserve memory
+		# as much as possible.  If you've passed through data that you
+		# know you won't need to look up the keys again, use this
+		# method to tell the KeyGenerator to forget that data.  It
+		# will not re-use the assigned keys, but will keep counting
+		# upward to assign new keys from here onward.  It will simply
+		# forget what all the existing keys map to.
+
+		del self.dataToKey
+		self.dataToKey = {}
+		gc.collect()
+		return
