ecf1b7cfb0550e09af8c86beaf5b59dfb319f03d
MGI Admin <mgiadmin@jax.org>
2014-05-21 19:31:33 +0000

    some queries still needed to check for unmapped emap terms

--- testdata/GXDTestData.py
+++ testdata/GXDTestData.py
@@ -72,43 +72,43 @@ Queries = [
 {	ID:"RNaseProtectionResultCount",
 	DESCRIPTION:"Assay Result count for RNaseProtection assay type",
 	SQLSTATEMENT:"""
-        select count(*) from gxd_expression ge, gxd_assaytype gt where ge._assaytype_key=gt._assaytype_key and ge.isforgxd=1 and gt.assaytype='RNase protection'
+        select count(*) from gxd_expression ge join tmp_emaps_ad ead on ead._structure_key=ge._structure_key, gxd_assaytype gt where ge._assaytype_key=gt._assaytype_key and ge.isforgxd=1 and gt.assaytype='RNase protection'
 	"""
 },
 {	ID:"WesternBlotResultCount",
 	DESCRIPTION:"Assay Result count for Western Blot assay type",
 	SQLSTATEMENT:"""
-        select count(*) from gxd_expression ge, gxd_assaytype gt where ge._assaytype_key=gt._assaytype_key and ge.isforgxd=1 and gt.assaytype='Western blot'
+        select count(*) from gxd_expression ge join tmp_emaps_ad ead on ead._structure_key=ge._structure_key, gxd_assaytype gt where ge._assaytype_key=gt._assaytype_key and ge.isforgxd=1 and gt.assaytype='Western blot'
 	"""
 },
 {	ID:"NorthernBlotResultCount",
 	DESCRIPTION:"Assay Result count for Northern Blot assay type",
 	SQLSTATEMENT:"""
-        select count(*) from gxd_expression ge, gxd_assaytype gt where ge._assaytype_key=gt._assaytype_key and ge.isforgxd=1 and gt.assaytype='Northern blot'
+        select count(*) from gxd_expression ge join tmp_emaps_ad ead on ead._structure_key=ge._structure_key, gxd_assaytype gt where ge._assaytype_key=gt._assaytype_key and ge.isforgxd=1 and gt.assaytype='Northern blot'
 	"""
 },
 {	ID:"NucleaseS1ResultCount",
 	DESCRIPTION:"Assay Result count for Nuclease S1 assay type",
 	SQLSTATEMENT:"""
-        select count(*) from gxd_expression ge, gxd_assaytype gt where ge._assaytype_key=gt._assaytype_key and ge.isforgxd=1 and gt.assaytype='Nuclease S1'
+        select count(*) from gxd_expression ge join tmp_emaps_ad ead on ead._structure_key=ge._structure_key, gxd_assaytype gt where ge._assaytype_key=gt._assaytype_key and ge.isforgxd=1 and gt.assaytype='Nuclease S1'
 	"""
 },
 {	ID:"RT-PCRResultCount",
 	DESCRIPTION:"Assay Result count for RT-PCR assay type",
 	SQLSTATEMENT:"""
-        select count(*) from gxd_expression ge, gxd_assaytype gt where ge._assaytype_key=gt._assaytype_key and ge.isforgxd=1 and gt.assaytype='RT-PCR'
+        select count(*) from gxd_expression ge join tmp_emaps_ad ead on ead._structure_key=ge._structure_key, gxd_assaytype gt where ge._assaytype_key=gt._assaytype_key and ge.isforgxd=1 and gt.assaytype='RT-PCR'
 	"""
 },
 {	ID:"InSituKnockInResultCount",
 	DESCRIPTION:"Assay Result count for In situ knockin assay type",
 	SQLSTATEMENT:"""
-        select count(*) from gxd_expression ge, gxd_assaytype gt where ge._assaytype_key=gt._assaytype_key and ge.isforgxd=1 and gt.assaytype='In situ reporter (knock in)'
+        select count(*) from gxd_expression ge join tmp_emaps_ad ead on ead._structure_key=ge._structure_key, gxd_assaytype gt where ge._assaytype_key=gt._assaytype_key and ge.isforgxd=1 and gt.assaytype='In situ reporter (knock in)'
 	"""
 },
 {	ID:"ImmunohistochemistryResultCount",
 	DESCRIPTION:"Assay Result count for Immunohistochemistry assay type",
 	SQLSTATEMENT:"""
-        select count(*) from gxd_expression ge, gxd_assaytype gt where ge._assaytype_key=gt._assaytype_key and ge.isforgxd=1 and gt.assaytype='Immunohistochemistry'
+        select count(*) from gxd_expression ge join tmp_emaps_ad ead on ead._structure_key=ge._structure_key, gxd_assaytype gt where ge._assaytype_key=gt._assaytype_key and ge.isforgxd=1 and gt.assaytype='Immunohistochemistry'
 	"""
 },
 {	ID:"RNAInSituResultCount",
