b4fa7098f22ee44abb0cc4ab2c18cc9825177fd7
Jonathan Beal <Jon.Beal@jax.org>
2014-09-25 16:39:02 +0000

    revised to use pre-computed rolled up annotations (type 1015)

--- gather/batch_marker_mp_annotations_gatherer.py
+++ gather/batch_marker_mp_annotations_gatherer.py
@@ -27,22 +27,23 @@ class BatchMarkerMPGatherer (Gatherer.Gatherer):
 ###--- globals ---###
 
 cmds = [
-	'''select distinct al._Marker_key,
-		ac.accID as mp_id,
-		mp.term as term
-	from all_allele al, gxd_allelegenotype gag, voc_term mp,
-		voc_annot ova, acc_accession ac
-	where al._Allele_key = gag._Allele_key
-		and gag._Genotype_key = ova._Object_key
-		and ova._Term_key = mp._Term_key
-		and ova._AnnotType_key = 1002
-		and ova._Qualifier_key != 2181424
-		and ova._Term_key = ac._Object_key
-		and ac._MGIType_key = 13
-		and ac.preferred = 1
-		and ac.prefixPart = 'MP:'
-		and al._Marker_key is not null
-	order by _Marker_key, mp_id'''
+	# 0. set of MP annotations that have been rolled up to markers (only
+	# keep null qualifiers, to avoid NOT and "normal" annotations).
+	'''select distinct va._Object_key as _Marker_key,
+		a.accID as mp_id,
+		t.term
+	from voc_annot va,
+		voc_term t,
+		acc_accession a,
+		voc_term q
+	where va._AnnotType_key = 1015
+		and va._Term_key = t._Term_key
+		and va._Term_key = a._Object_key
+		and a._MGIType_key = 13
+		and a.preferred = 1
+		and va._Qualifier_key = q._Term_key
+		and q.term is null
+	order by va._Object_key, a.accID''',
 	]
 
 # order of fields (from the query results) to be written to the
