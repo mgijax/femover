7850fd418eaffde756a332af9e4974d56b44cb3e
Jonathan Beal <Jon.Beal@jax.org>
2015-05-18 11:54:35 +0000

    added new CachingMultiFileGatherer and more debugging info re: memory usage

--- gather/Gatherer.py
+++ gather/Gatherer.py
@@ -12,6 +12,7 @@ import logger
 import types
 import dbAgnostic
 import OutputFile
+import top
 
 ###--- Globals ---###
 
@@ -22,7 +23,16 @@ SOURCE_DB = config.SOURCE_TYPE	# either sybase, mysql, or postgres
 # cache of terms already looked up
 resolveCache = {}		# resolveCache[table][keyField][key] = term
 
+# top.Process object for the currently executing script
+myProcess = top.getMyProcess()
+
 ###--- Functions ---###
+
+def myMemory():
+	# Return as a string the current memory used by the current process
+
+	myProcess.measure()
+	return top.displayMemory(myProcess.getLatestMemoryUsed())
 
 def resolve (key,		# integer; key value to look up
 	table = "voc_term",	# string; table in which to look up key
@@ -100,6 +110,7 @@ def executeQueries (cmds):
 		if results[-1][1]:
 			count = len(results[-1][1])
 		logger.debug ('Finished query %d (%d results)' % (i, count))
+		logger.debug ('RAM used : %s' % myMemory())
 		i = i + 1
 	return results
 
@@ -123,6 +134,17 @@ def main (
 	logger.info ('Begin %s' % sys.argv[0])
 	gatherer.go()
 	logger.close()
+	return
+
+def logMemoryUsage():
+	# Purpose: write to the log file the current memory usage for
+	#	the script
+	# Returns: nothing
+	# Assumes: nothing
+	# Modifies: writes to log file
+	# Throws: nothing
+
+	logger.info ('RAM used : %s' % myMemory())
 	return
 
 ###--- Classes ---###
@@ -426,14 +448,21 @@ class MultiFileGatherer:
 
 		self.preprocessCommands()
 		logger.info ('Pre-processed queries')
+		logMemoryUsage()
+
 		if self.cmds:
 			self.results = executeQueries (self.cmds)
 			logger.info ('Finished queries of source %s db' % \
 				SOURCE_DB)
+			logMemoryUsage()
+
 		self.collateResults()
 		logger.info ('Built %d result sets' % len(self.output))
+		logMemoryUsage()
+
 		self.postprocessResults()
 		logger.info ('Post-processed result sets')
+		logMemoryUsage()
 
 		# if the gatherer is managing its own writes, then we can just
 		# bail out now 
@@ -459,6 +488,215 @@ class MultiFileGatherer:
 		for (filename, fieldOrder, tableName) in self.files:
 			items.append (tableName)
 		return items
+
+	def collateResults (self):
+		# Purpose: to do any necessary slicing and dicing of
+		#	self.results to produce a final set of results to be
+		#	written in self.output
+		# Returns: nothing
+		# Assumes: nothing
+		# Modifies: nothing
+		# Throws: nothing
+
+		raise Error, 'Must define collateResults() in subclass'
+
+	def postprocessResults (self):
+		# Purpose: to do any necessary post-processing of the final
+		#	results before they are written out to files
+		# Returns: nothing
+		# Assumes: nothing
+		# Modifies: nothing
+		# Throws: nothing
+
+		return
+
+class CachingMultiFileGatherer:
+	# Is: a Gatherer which handles generating multiple files rather than a
+	#	single one.  This is useful for cases where we are generating
+	#	unique keys which need to be used for joins to related tables.
+	#	This class is distinguished from a traditional MultiFileGatherer
+	#	in that it uses CachingOutputFile objects to store only a
+	#	portion of the results in memory, while automatically writing
+	#	the others out to disk. This should be a substantial memory
+	#	savings for some data sets.
+	# Has: information about queries to execute, fields to retrieve, how
+	#	to write data files, etc.
+	# Does: queries source db for data, collates result sets, and writes
+	#	out one or more text files of results
+	# Notes: This is not a subclass of Gatherer, because it is pretty much
+	#	completely re-implemented.  They share a common goal (extract
+	#	data from the source database, repackage it, and write data
+	#	files), but that is as far as the similarity goes.
+
+	def __init__ (self,
+		files,			# list of (table name, order of fields
+					# as submitted, order of fields to be
+					# written out) tuples : 1 per table
+		cmds = None		# list of strings; queries to execute
+					# ...against the source database to
+					# ...extract data
+		):
+		self.baseCmds = cmds[:]
+
+		self.results = []	# list of lists of query results (to
+					# ...be built from executing 'cmds')
+
+		# manages our CachingOutputFiles
+		self.files = OutputFile.CachingOutputFileFactory()
+
+		# maps from table name (string) to the file ID in self.files
+		self.tablenameToFileID = {}
+
+		for (tableName, inFieldOrder, outFieldOrder) in files:
+			fileID = self.files.createFile(tableName, inFieldOrder,
+				outFieldOrder, OutputFile.MEDIUM_CACHE)
+			self.tablenameToFileID[tableName] = fileID
+
+		logger.debug('Set up %d CachingOutputFiles' % len(files))
+
+		# chunking is optional, but can keep memory down for those 
+		# data sets where the queries can be walked through a few keys
+		# at a time.  Use the setupChunking() method to set these up
+		# before calling go().
+
+		self.chunkingOn = False
+		self.minKey = None
+		self.maxKey = None
+		self.chunkSize = None
+		return
+
+	def setupChunking (self,
+		minKeyQuery,		# string; SQL to get minimum key
+		maxKeyQuery,		# string; SQL to get maximum key
+		chunkSize = 10000	# int; number of keys to do at once
+		):
+		# Purpose: set up this gatherer to walk through its data in
+		#	chunks, rather than doing all the results at once
+		# Returns: nothing
+		# Assumes: both 'minKeyQuery' and 'maxKeyQuery' return a
+		#	single row with a single value
+		# Modifies: four instance variables
+		# Throws: propagates any exceptions if there are problems
+		#	executing either 'minKeyQuery' or 'maxKeyQuery'
+		#	against the database
+
+		cols, rows = dbAgnostic.execute(minKeyQuery)
+		self.minKey = rows[0][0]
+
+		cols, rows = dbAgnostic.execute(maxKeyQuery)
+		self.maxKey = rows[0][0]
+
+		self.chunkSize = chunkSize
+		self.chunkingOn = True
+
+		logger.debug('Chunking on; keys %d to %d' % (self.minKey,
+			self.maxKey))
+		return
+
+	def preprocessCommands (self):
+		# Purpose: to do any necessary pre-processing of the SQL
+		#	queries
+		# Returns: nothing
+		# Assumes: nothing
+		# Modifies: nothing
+		# Throws: nothing
+
+		return
+
+	def preprocessCommandsByChunk (self, lowKey, highKey):
+		# The commands to be updated initially live in self.baseCmds.
+		# For any which include a '%d', then we fill in the lowKey and
+		# highKey in their places.  Updated commands are placed in 
+		# self.cmds.
+
+		self.cmds = []
+
+		for cmd in self.baseCmds:
+			if '%d' in cmd:
+				self.cmds.append (cmd % (lowKey, highKey))
+			else:
+				self.cmds.append (cmd)
+		return
+
+	def getFileID (self, tableName):
+		# returns the file ID associated with the tableName
+
+		if self.tablenameToFileID.has_key(tableName):
+			return self.tablenameToFileID[tableName]
+		raise Error, 'Unknown table name: %s' % tableName
+
+	def setCacheSize (self, fileID, cacheSize):
+		# adjust the cache size for the file with the given fileID
+
+		self.files.setCacheSize(fileID, cacheSize)
+		return
+
+	def addRow (self, fileID, row):
+		# adds the given row to the data file identified by fileID
+
+		self.files.addRow(fileID, row)
+		return
+
+	def addRows (self, fileID, rows):
+		# adds the given list of rows to the data file identified by
+		# fileID
+
+		self.files.addRows(fileID, rows)
+		return
+
+	def go (self):
+		# Purpose: to drive the gathering process from queries
+		#	through writing the output file
+		# Returns: nothing
+		# Assumes: nothing
+		# Modifies: queries the database, writes to the file system;
+		#	writes one line to stdout for each file written,
+		#	containing the path to the file and the table into
+		#	which it should be loaded
+		# Throws: propagates all exceptions
+
+		self.preprocessCommands()
+		logger.info ('Pre-processed queries')
+		logMemoryUsage()
+
+		if self.chunkingOn:
+			lowKey = self.minKey
+			while lowKey <= self.maxKey:
+				highKey = lowKey + self.chunkSize
+				self.results = []
+
+				self.preprocessCommandsByChunk(lowKey, highKey)
+				self.results = executeQueries(self.cmds)
+				self.collateResults()
+				self.postprocessResults()
+
+				logger.debug('Handled keys %d-%d' % (lowKey,
+					highKey - 1))
+				lowKey = highKey 
+				logMemoryUsage()
+		else:
+			# no chunking -- process all results at once
+
+			if self.baseCmds:
+			    self.results = executeQueries (self.baseCmds)
+			    logger.info ('Finished queries of source %s db' % \
+				SOURCE_DB)
+
+			self.collateResults()
+			logger.info ('Collated results')
+			logMemoryUsage()
+
+			self.postprocessResults()
+			logger.info ('Post-processed results')
+			logMemoryUsage()
+
+		self.files.closeAll()
+		logger.info ('Closed all output files')
+		logMemoryUsage()
+
+		self.files.reportAll()
+		logger.info ('Reported file info to stdout')
+		return
 
 	def collateResults (self):
 		# Purpose: to do any necessary slicing and dicing of
