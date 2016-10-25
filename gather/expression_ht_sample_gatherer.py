114fef0eb3a9eece8f6d673e747a50807a52cea9
Jonathan Beal <jsb@jax.org>
2016-10-25 12:10:19 -0400

    restructuted so fake data and real data use same pathway; added computation of ageMin and ageMax from age string

--- gather/expression_ht_sample_gatherer.py
+++ gather/expression_ht_sample_gatherer.py
@@ -4,6 +4,8 @@
 
 import Gatherer
 import logger
+import VocabSorter
+import AgeUtils
 from expression_ht import samples
 
 ###--- Functions ---###
@@ -43,18 +45,128 @@ def getCharacteristic(sample, name):
 					return characteristic['value'] 
 	return None
 
-import re
-ageRegex = re.compile('^E([0-9]{1,2}\.?[0-9]?)$')
-def getAge(ageString):
-	# returns (age as a string, ageMin, ageMax)
-	if not ageString:
-		return None, 24, 28
+#import re
+#ageRegex = re.compile('^E([0-9]{1,2}\.?[0-9]?)$')
+#def getAge(ageString):
+#	# returns (age as a string, ageMin, ageMax)
+#	if not ageString:
+#		return None, 24, 28
+#	
+#	match = ageRegex.match(str(ageString))
+#	if not match:
+#		return ageString, 24, 28
+#	
+#	return ageString, float(match.group(1)), float(match.group(1))
+
+###--- functions to keep ---###
+
+expKeyCol = None
+relevanceCol = None
+ageMinCol = None
+ageMaxCol = None
+structureCol = None
+tsCol = None
+organismCol = None
+nameCol = None
+def cacheColumns(cols):
+	global expKeyCol, relevanceCol, ageMaxCol, ageMinCol, structureCol, tsCol, organismCol, nameCol
 	
-	match = ageRegex.match(str(ageString))
-	if not match:
-		return ageString, 24, 28
+	expKeyCol = Gatherer.columnNumber(cols, '_Experiment_key')
+	relevanceCol = Gatherer.columnNumber(cols, 'relevancy')
+	ageMaxCol = Gatherer.columnNumber(cols, 'ageMax')
+	ageMinCol = Gatherer.columnNumber(cols, 'ageMin')
+	structureCol = Gatherer.columnNumber(cols, '_EMAPA_key')
+	tsCol = Gatherer.columnNumber(cols, 'theiler_stage')
+	organismCol = Gatherer.columnNumber(cols, 'organism')
+	nameCol = Gatherer.columnNumber(cols, 'name')
+
+	logger.debug('Cached 8 column indices')
+	return
+
+preferredOrganisms = {
+	'mouse, laboratory' : 1,
+	'human' : 2,
+	'rat'	: 3,
+	}
+def compareSamples(a, b):
+	# sort samples by:
+	#	0. experiment key, 1. relevance (yes first), 2. ageMin, 3. ageMax, 4. structure (topologically),
+	#   5. Theiler stage, 6. organism (mouse, human, rat, others), 7. sample name
+	# assumes: cacheColumns() was called previously
 	
-	return ageString, float(match.group(1)), float(match.group(1))
+	byExpKey = cmp(a[expKeyCol], b[expKeyCol])				# experiment key
+	if byExpKey != 0:
+		return byExpKey
+
+	byRelevance = cmp(a[relevanceCol], b[relevanceCol]) * -1	# relevance in descending order: yes, no, None
+	if byRelevance != 0:
+		return byRelevance
+
+	if a[ageMinCol]:								# ageMin
+		if b[ageMinCol]:
+			byAgeMin = cmp(a[ageMinCol], b[ageMinCol])
+			if byAgeMin != 0:
+				return byAgeMin 
+		else:
+			return -1
+	elif b[ageMinCol]:
+		return 1
+		
+	if a[ageMaxCol]:								# ageMax
+		if b[ageMaxCol]:
+			byAgeMax = cmp(a[ageMaxCol], b[ageMaxCol])
+			if byAgeMax != 0:
+				return byAgeMax
+		else:
+			return -1
+	elif b[ageMaxCol]:
+		return 1
+
+	if a[structureCol]:								# structure, sorted topologically
+		if b[structureCol]:
+			byStructure = cmp(VocabSorter.getSequenceNum(a[structureCol]), VocabSorter.getSequenceNum(b[structureCol]))
+			if byStructure != 0:
+				return byStructure
+		else:
+			return -1
+	elif b[structureCol]:
+		return 1
+	
+	if a[tsCol]:								# Theiler Stage
+		if b[tsCol]:
+			byTS = cmp(int(a[tsCol]), int(b[tsCol]))
+			if byTS != 0:
+				return byTS
+		else:
+			return -1
+	elif b[tsCol]:
+		return 1
+	
+	if a[organismCol]:							# organism, preferred organisms above, others alphabetical
+		if b[organismCol]:
+			if a[organismCol] in preferredOrganisms:
+				if b[organismCol] in preferredOrganisms:
+					byOrg = cmp(preferredOrganisms[a[organismCol]], preferredOrganisms[b[organismCol]])
+					if byOrg != 0:
+						return byOrg
+				else:
+					return -1
+			elif b[organismCol] in preferredOrganisms:
+				return 1
+		else:
+			return -1
+	elif b[organismCol]:
+		return 1
+	
+	if a[nameCol]:								# sample name
+		if b[nameCol]:
+			return cmp(str(a[nameCol]).lower(), str(b[nameCol]).lower())
+		else:
+			return -1
+	elif b[nameCol]:
+		return 1
+
+	return 0
 
 ###--- Classes ---###
 
@@ -64,20 +176,14 @@ class HTSampleGatherer (Gatherer.Gatherer):
 	# Does: queries the source database for primary data for high-throughput expression
 	#	samples, collates results, writes tab-delimited text file
 
-	def collateResults(self):
+	def getFakeData(self):
 		# TODO -- remove this method and go back to the superclass method; This is a hack until we have
 		# curated sample data.
 		
-		cols, rows = self.results[0]
-		if rows:
-			return Gatherer.Gatherer.collateResults(self)
-		
-		self.finalColumns = [
-			'_Sample_key', '_Experiment_key', 'name', '_Genotype_key', 'organism', 'sex', 'age',
-			'ageMin', 'ageMax', '_EMAPA_key', 'theiler_stage', 'relevancy', 'sequence_num',
-			]
-		self.finalResults = []
-		
+		cols = [ '_Sample_key', '_Experiment_key', 'name', '_Genotype_key', 'organism', 'sex', 'age',
+			'_EMAPA_key', 'theiler_stage', 'relevancy', ]
+		rows = []
+
 		sampleKey = 0
 		for experimentID in samples.getExperimentsWithSamples():
 			experimentKey = samples.getExperimentKey(experimentID)
@@ -102,8 +208,10 @@ class HTSampleGatherer (Gatherer.Gatherer):
 				row.append(cleanUpOrganism(getCharacteristic(sample, 'organism')))
 				row.append(getCharacteristic(sample, 'sex'))
 
-				age, ageMin, ageMax = getAge(getCharacteristic(sample, 'age'))
-				row = row + [ age, ageMin, ageMax ]
+#				age, ageMin, ageMax = getAge(getCharacteristic(sample, 'age'))
+#				row = row + [ age, ageMin, ageMax ]
+				age = getCharacteristic(sample, 'age')
+				row.append(age)
 
 				emapaKey, startStage = samples.getEmapa(getCharacteristic(sample, 'organism part'))
 				row.append(emapaKey)
@@ -114,19 +222,62 @@ class HTSampleGatherer (Gatherer.Gatherer):
 				else:
 					row.append("Non-mouse sample: no data stored")
 
-				row.append(sampleKey)		# sequence num
-				self.finalResults.append(row)
+				rows.append(row)
 				
 		logger.debug("Hacked in %d sample rows" % len(self.finalResults))
+		return cols, rows
+
+	def addAgeMinMax(self):
+		# compute and add the ageMin and ageMax fields, adding them to the list of 'cols' and to each
+		# sample in 'rows'
+		
+		self.finalColumns.append('ageMin')
+		self.finalColumns.append('ageMax')
+		ageCol = Gatherer.columnNumber(self.finalColumns, 'age')
+		
+		for row in self.finalResults:
+#			age, ageMin, ageMax = getAge(row[ageCol])
+			age, ageMin, ageMax = AgeUtils.getAgeMinMax(row[ageCol])
+			row[ageCol] = age
+			row.append(ageMin)
+			row.append(ageMax)
+
+		logger.debug('Computed ageMin/Max for %d samples' % len(self.finalResults))
+		return
+	
+	def applySequenceNumbers(self):
+		# re-order the given sample rows and append a sequence number for each
+
+		cacheColumns(self.finalColumns)
+		self.finalResults.sort(compareSamples)
+		logger.debug('Sorted %d samples' % len(self.finalResults))
+
+		i = 0
+		self.finalColumns.append('sequence_num')
+		for sample in self.finalResults:
+			i = i + 1
+			sample.append(i)
+		logger.debug('Applied sequence numbers')
+
+		return samples
+
+	def collateResults(self):
+		cols, rows = self.results[0]
+		if not rows:
+			cols, rows = self.getFakeData()
+		
+		self.finalColumns = cols
+		self.finalResults = rows
+		
+		self.addAgeMinMax()
+		self.applySequenceNumbers()
 		return
 		
 ###--- globals ---###
 
 cmds = [
 	'''select t._Experiment_key, r.term as relevancy, t._Sample_key, s.name, s._Genotype_key,
-			o.commonName as organism, x.term as sex, s.age, s.ageMin, s.ageMax, s._Emapa_key,
-			g.stage as theiler_stage,
-			row_number() over (order by t._Experiment_key, s.name) as sequence_num
+			o.commonName as organism, x.term as sex, s.age, s._Emapa_key, g.stage as theiler_stage
 		from %s t, gxd_htsample s, voc_term r, mgi_organism o, voc_term x, gxd_theilerstage g
 		where t._Sample_key = s._Sample_key
 			and s._Organism_key = o._Organism_key
---
+++ lib/python/AgeUtils.py
@@ -0,0 +1,205 @@
+# Name: AgeUtils.py
+# Purpose: handle various age transformations as we move data from back-end to front-end
+
+import re
+
+embryonicAbbrev = re.compile('^e([0-9.]+)$')
+embryonicSolo = re.compile('^e(mbryonic)? *(day)? *([0-9.]+)$')
+embryonicPair = re.compile('^e(mbryonic)? *(day)? *([0-9.]+)-([0-9.]+)$')
+embryonicTriple = re.compile('^e(mbryonic)? *(day)? *([0-9.]+), *([0-9.]+), *([0-9.]+)$')
+
+postnatalAbbrev = re.compile('^p([0-9.]+)$')
+postnatalSolo = re.compile('^p(ostnatal)? ([a-z]+) *([0-9.]+)$')
+postnatalPair = re.compile('^p(ostnatal)? ([a-z]+) *([0-9.]+)-([0-9.]+)$')
+postnatalTriple = re.compile('^p(ostnatal)? ([a-z]+) *([0-9.]+), *([0-9.]+), *([0-9.]+)$')
+
+fixedRanges = {
+    'embryonic' : ('E', 0.0, 21.0),
+    'perinatal' : ('Perinatal', 17.0, 22.0),
+    'postnatal' : ('P', 21.01, 1846.0),
+    'postnatal newborn' : ('P newborn', 21.01, 25.0),
+    'postnatal immature' : ('P immature', 25.01, 42.0),
+    'postnatal adult' : ('P adult', 42.01, 1846.0),
+    'p newborn' : ('P newborn', 21.01, 25.0),
+    'p immature' : ('P immature', 25.01, 42.0),
+    'p adult' : ('P adult', 42.01, 1846.0),
+    }
+
+multiplier = {
+    'd' : 1,
+    'day' : 1,
+    'days' : 1,
+    'w' : 7,
+    'wk' : 7,
+    'wks' : 7,
+    'week' : 7,
+    'weeks' : 7,
+    'm' : 30,
+    'month' : 30,
+    'months' : 30,
+    'y' : 365,
+    'year' : 365,
+    'years' : 365,
+    }
+
+def getAgeMinMax(age):
+    # convert the textual age to its ageMin and ageMax equivalents, if the format is recognized
+    # Returns: (ageMin, ageMax) or (None, None) if age cannot be converted
+    
+    ageString = str(age).lower().strip()
+    ageString = tweakAge(ageString)           # TODO: remove once we have curated samples
+    
+    if ageString in fixedRanges:
+        return fixedRanges[ageString]
+
+    abbrev, ageMin, ageMax = _getEmbryonicMinMax(ageString)
+    if ageMin:
+        return abbrev, ageMin, ageMax
+    
+    abbrev, ageMin, ageMax = _getPostnatalMinMax(ageString)
+    if ageMin:
+        return abbrev, ageMin, ageMax
+
+    return age, None, None
+
+def tweakAge(age):
+    # tries to intelligently adapt a non-standard age format to one of our standard age formats
+    # TODO: remove this once we start having curated samples
+    
+    ageString = age.lower().strip().replace(' to ', '-').replace('post natal', 'postnatal').replace(' old', '').replace(' since birth', '')
+
+    solo = re.compile('^([0-9.]+) ([a-z]+)s$')    # 123 units (postnatal implied)
+    match = solo.match(ageString)
+    if match:
+        rawAge, unit = match.groups()
+        if unit in multiplier:
+            try:
+                ageMin = float(rawAge)
+                return 'postnatal %s %s' % (unit, rawAge)
+            except:
+                pass
+    
+    pair = re.compile('^([0-9.]+)-([0-9.]+) ([a-z]+)s$')      # 123-456 units (postnatal implied)
+    match = pair.match(ageString)
+    if match:
+        rawMin, rawMax, unit = match.groups()
+        if unit in multiplier:
+            try:
+                ages = map(float, [ rawMin, rawMax ])
+                return 'postnatal %s %s-%s' % (unit, rawMin, rawMax)
+            except:
+                pass
+            
+    return age
+    
+def _getEmbryonicMinMax(age):
+    # handle the embryonic ages, returning min/max for 'age' if it is recognized
+
+    match = embryonicAbbrev.match(age)        # ex
+    if match:
+        try:
+            fAge = float(match.group(1))
+            return 'E%s' % match.group(1), fAge, fAge
+        except:
+            pass
+
+    match = embryonicSolo.match(age)        # embryonic day x
+    if match:
+        try:
+            fAge = float(match.group(-1))
+            return 'E%s' % match.group(-1), fAge, fAge
+        except:
+            pass
+
+    match = embryonicPair.match(age)        # embryonic day x-y
+    if match:
+        try:
+            pair = map(float, match.groups()[-2:])
+            return 'E%s-%s' % (pair[0], pair[1]), min(pair), max(pair)
+        except:
+            pass                            # non-floats; fall through to None, None
+    
+    match = embryonicTriple.match(age)      # embryonic day x, y, z
+    if match:
+        try:
+            triple = map(float, match.groups()[-3:])
+            return 'E%s,%s,%s' % (triple[0], triple[1], triple[2]), min(triple), max(triple)
+        except:
+            pass                            # non-floats; fall through to None, None
+    
+    return age, None, None
+
+def _getPostnatalMinMax(age):
+    # handle the various 'postnatal' options
+
+    fMin = None         # float rawMin (before multiplier)
+    fMax = None         # float rawMax (before multiplier)
+    days = None         # number of days for age unit
+    
+    abbrev = age
+
+    match = postnatalAbbrev.match(age)        # px
+    if match:
+        try:
+            fMin = float(match.group(1))
+            fMax = fMin
+            days = 1
+        except:
+            pass
+
+    if (days == None) or (fMin == None) or (fMax == None):
+        match = postnatalSolo.match(age)
+        if match:
+            (skip, unit, rawAge) = match.groups()
+            if unit in multiplier:
+                days = multiplier[unit]
+                try:
+                    fMin = float(rawAge)
+                    fMax = fMin
+                    if unit == 'day':
+                        abbrev = 'P%s' % rawAge
+                    else:
+                        abbrev = 'P %s%s' % (unit[0], rawAge)
+                except:
+                    pass
+
+    if (days == None) or (fMin == None) or (fMax == None):
+        match = postnatalPair.match(age)
+        if match:
+            (skip, unit, rawMin, rawMax) = match.groups()
+            if unit in multiplier:
+                days = multiplier[unit]
+                try:
+                    pair = map(float, [ rawMin, rawMax ] )
+                    fMin = min(pair)
+                    fMax = max(pair)
+                    
+                    if unit == 'day':
+                        abbrev = 'P%s-%s' % (rawMin, rawMax)
+                    else:
+                        abbrev = 'P %s%s-%s' % (unit[0], rawMin, rawMax)
+                except:
+                    pass                        # non-floats; fall through to None, None
+                
+    if (days == None) or (fMin == None) or (fMax == None):
+        match = postnatalTriple.match(age)
+        if match:
+            (skip, unit, x, y, z) = match.groups()
+            if unit in multiplier:
+                days = multiplier[unit]
+                try:
+                    triple = map(float, [ x, y, z ])
+                    fMin = min(triple)
+                    fMax = max(triple)
+                    
+                    if unit == 'day':
+                        abbrev = 'P%s,%s,%s' % (x, y, z)
+                    else:
+                        abbrev = 'P %s%s,%s,%s' % (unit[0], x, y, z)
+                except:
+                    pass
+
+    if (fMin != None) and (fMax != None) and (days != None):
+        return abbrev, (days * fMin + 21.01), (days * fMax + 21.01)
+        
+    return age, None, None
