2d919d3df2b69785816ee577b60f1880b1cab4df
Olin Blodgett <nilo@nilosplace.net>
2016-11-28 15:45:18 -0500

    Final Clean up of conversion code

--- control/postgresConverter.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Purpose: currently a no-op; in the future, if processing of text files is
-#	needed before loading them into Postgres, this is where it would go
-#
-# this used to do some sybase-to-postgres conversions, but now it does nothing
-
-# just report back the filename that should be loaded
-echo $1
