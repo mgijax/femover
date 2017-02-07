3b368a23a31d420d5bdc74902c1fcc4b444d829c
Lori Corbani <lori.corbani@jax.org>
2017-02-07 07:39:12 -0500

    DO changes/disease_detail_gatherer

--- schema/disease_group_row.py
+++ schema/disease_group_row.py
@@ -18,6 +18,7 @@ createStatement = '''CREATE TABLE %s  (
 	disease_group_key	int		not null,
 	disease_row_key		int		not null,
 	disease_key		int		not null,
+	disease		        text		not null,
 	PRIMARY KEY(disease_group_row_key))''' % tableName
 
 # Maps from index suffix to create statement for that index.  In each
