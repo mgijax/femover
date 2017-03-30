79dfbae7fe2984c6457136b693e855a0c41f51d2
Lori Corbani <lori.corbani@jax.org>
2017-03-30 11:25:38 -0400

    TR12540/Disease Ontology (DO)

--- postprocess/disease_detail_gatherer.sh
+++ postprocess/disease_detail_gatherer.sh
@@ -13,7 +13,7 @@ psql -h ${PG_FE_DBSERVER} -U ${PG_FE_DBUSER} -d ${PG_FE_DBNAME} <<EOSQL
 WITH tmp_genes_counts AS (
 SELECT gr.disease_key, count(gr.disease_key) AS genes_count
 FROM disease_row_to_marker dm, disease_row r, disease_group_row gr, disease_group dg
-        WHERE dm.disease_row_key = r.disease_row_key
+WHERE dm.disease_row_key = r.disease_row_key
         AND r.disease_row_key = gr.disease_row_key
         AND gr.disease_group_key = dg.disease_group_key
         AND dg.group_type in ('mouse only', 'human only', 'mouse and human')
@@ -30,14 +30,23 @@ select count(*) from disease where genes_tab_count > 0;
 
 -- update disease.models_tab_count
 
-WITH tmp_models_counts AS (
-SELECT gr.disease_key, count(gr.disease_key) AS models_count
-FROM disease_row_to_model dm, disease_row r, disease_group_row gr, disease_group dg
-        WHERE dm.disease_row_key = r.disease_row_key
+SELECT gr.disease_key
+INTO TEMP tmp_models
+FROM disease_row_to_model drm, disease_row r, disease_group_row gr, disease_group dg, disease_model dm, genotype g        
+WHERE drm.disease_row_key = r.disease_row_key
         AND r.disease_row_key = gr.disease_row_key
         AND gr.disease_group_key = dg.disease_group_key
-        AND dg.group_type in ('mouse only', 'human only', 'mouse and human', 'additional')
-        GROUP BY gr.disease_key
+        AND dg.group_type in ('mouse only', 'human only', 'mouse and human', 'additional', 'non-genes')
+        AND drm.disease_model_key = dm.disease_model_key
+        AND dm.is_not_model = 0
+        AND dm.genotype_key = g.genotype_key
+        AND g.is_disease_model = 1
+        GROUP BY gr.disease_key, g.background_strain, g.combination_1
+;
+WITH tmp_models_counts AS (
+SELECT disease_key, count(disease_key) as models_count
+FROM tmp_models
+GROUP BY disease_key
 )
 UPDATE disease
 SET models_tab_count = tmp_models_counts.models_count
