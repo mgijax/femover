a0f1b0d4124257b5a43f0da17ca4456ee84a5bb7
Jonathan Beal <jsb@jax.org>
2017-04-04 11:27:33 -0400

    added public comments for MP terms

--- gather/term_note_gatherer.py
+++ gather/term_note_gatherer.py
@@ -15,18 +15,16 @@ TermNoteGatherer = Gatherer.Gatherer
 ###--- globals ---###
 
 cmds = [
-	# 0. start with comments for GO terms
-	'''select tt._Term_key, c.note, case 
-			when t.noteType = 'Public Vocabulary Term Comment' then 'Comment'
-			else t.noteType
-			end as noteType
+	# 0. start with public comments for GO terms and MP terms
+	'''select tt._Term_key, c.note, 'Comment' as noteType
 		from mgi_notetype t, mgi_note n, voc_term tt, voc_vocab v, mgi_notechunk c
 		where t._MGIType_key = 13
 			and t._NoteType_key = n._NoteType_key
 			and n._Object_key = tt._Term_key
 			and n._Note_key = c._Note_key
 			and tt._Vocab_key = v._Vocab_key
-			and v.name = 'GO' ''',
+			and t.noteType = 'Public Vocabulary Term Comment'
+			and v.name in ('GO', 'Mammalian Phenotype') ''',
 	]
 
 # order of fields (from the query results) to be written to the
