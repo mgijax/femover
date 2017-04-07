291dc4eb77e9e04d9ee6dfe57d4c264016e738ad
Lori Corbani <lori.corbani@jax.org>
2017-04-07 11:22:00 -0400

    TR12540/Disease Ontology (DO)

--- postprocess/disease_detail_gatherer.sh
+++ postprocess/disease_detail_gatherer.sh
@@ -10,15 +10,20 @@ psql -h ${PG_FE_DBSERVER} -U ${PG_FE_DBUSER} -d ${PG_FE_DBNAME} <<EOSQL
 
 -- update disease.genes_tab_count
 
-WITH tmp_genes_counts AS (
-SELECT gr.disease_key, count(gr.disease_key) AS genes_count
+SELECT gr.disease_key
+INTO TEMP tmp_genes
 FROM disease_row_to_marker dm, disease_row r, disease_group_row gr, disease_group dg
 WHERE dm.disease_row_key = r.disease_row_key
         AND r.disease_row_key = gr.disease_row_key
         AND gr.disease_group_key = dg.disease_group_key
         AND dg.group_type in ('mouse only', 'human only', 'mouse and human')
         AND dm.is_causative = 1
-        GROUP BY gr.disease_key
+        GROUP BY gr.disease_key, dm.marker_key
+;
+WITH tmp_genes_counts AS (
+SELECT disease_key, count(disease_key) as genes_count
+FROM tmp_genes
+GROUP BY disease_key
 )
 UPDATE disease
 SET genes_tab_count = tmp_genes_counts.genes_count
