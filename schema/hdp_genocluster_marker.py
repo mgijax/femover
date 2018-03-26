6e978e649c8ba9a8751ca9861f70a8bc4dfbbfaa
Jonathan Beal <jsb@jax.org>
2018-03-26 15:55:07 -0400

    split hdp_genocluster_marker join table out of hdp_genocluster table to allow true many-to-many relationship between markers and genoclusters

--- schema/hdp_genocluster_marker.py
+++ schema/hdp_genocluster_marker.py
@@ -2,7 +2,7 @@
 
 import Table
 
-# contains data definition information for the hdp_genocluster table
+# contains data definition information for the hdp_genocluster_marker table
 
 # Note: All table and field names should be all-lowercase with underscores
 # used to separate words.
@@ -11,39 +11,48 @@ import Table
 
 # name of this database table
 # human disease portal
-tableName = 'hdp_genocluster'
+tableName = 'hdp_genocluster_marker'
+
 #
 # statement to create this table
 #
-# hdp (human disease portal)
-#
-# This table represents the unique "geno-cluster" key.
-#
-# see the gatherer for more information
+# This table represents the relationship between markers and genoclusters
 #
 createStatement = '''CREATE TABLE %s  ( 
 	unique_key              int     not null,
 	hdp_genocluster_key		int		not null,
+	marker_key				int		not null,
+	symbol					text	null,
+	primary_id				text	null,
+	marker_type				text	null,
 	PRIMARY KEY(unique_key))''' % tableName
 
 # Maps from index suffix to create statement for that index.  In each
 # statement, the first %s is for the index name, and the second is for the
 # table name.
 indexes = {
-        'hdp_genocluster_key' : 'create unique index %s on %s (hdp_genocluster_key)',
+        'marker_key' : 'create index %s on %s (marker_key)',
+        'primary_id' : 'create index %s on %s (primary_id)',
 	}
 
-keys = {}
+keys = {
+        'marker_key' : ('marker', 'marker_key'),
+        'hdp_genocluster_key' : ('hdp_genocluster', 'hdp_genocluster_key'),
+	}
 
 # index used to cluster data in the table
-clusteredIndex = None
+clusteredIndex = ('hdp_genocluster_key', 'create index %s on %s (hdp_genocluster_key)')
 
 # comments describing the table, columns, and indexes
 comments = {
-	Table.TABLE : 'central table for the genoclusetr petal, containing one row for each human disease portal genocluster',
+	Table.TABLE : 'join table between genoclusters and markers, allowing for multiple markers per genocluster (common for transgenes)',
 	Table.COLUMN : {
 		'unique_key' : 'unique key for this record',
-		'hdp_genocluster_key' : 'unique key identifying this human disease portal genotype cluster',
+		'hdp_genocluster_key' : 'foreign key to hdp_genocluster table',
+		'marker_key' : 'foreign key to marker table',
+		'symbol' : 'marker symbol',
+		'primary_id' : 'primary accession ID of marker',
+		'marker_type' : 'type of marker',
 		},
 	}
 
