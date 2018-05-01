97ebd87530257a620ae3166ca6bee0890a9ccd01
Jonathan Beal <jsb@jax.org>
2018-04-30 22:48:35 -0400

    added interaction with IMSR for strain data

--- lib/python/IMSRData.py
+++ lib/python/IMSRData.py
@@ -4,6 +4,7 @@
 import logger
 import urllib2
 import config
+import gc
 
 class IMSRDatabase:
 	IMSR_COUNT_URL = 'IMSR_COUNT_URL' in dir(config) and config.IMSR_COUNT_URL or ""
@@ -71,11 +72,70 @@ class IMSRDatabase:
 
 		return cellLines, strains, byMarker
 
+class IMSRStrainData:
+	IMSR_STRAIN_URL = 'IMSR_STRAIN_URL' in dir(config) and config.IMSR_STRAIN_URL or ""
+
+	# Issues a query to the IMSR WI to retrieve a report mapping from mouse strain names to
+	# the IDs IMSR recognizes for them (and other data, too).  This report is fairly big (125 MB).
+	# cellLineCount, strainCount, totalCountForMarker
+	# Returns: a list of tuples, each including:
+	#		(strain name or synonym, approved nomen flag (0/1), IMSR ID, repository, source URL)
+	def queryStrains (self):
+		logger.debug ('IMSR_STRAIN_URL : %s' % self.IMSR_STRAIN_URL)
+
+		f = urllib2.urlopen(self.IMSR_STRAIN_URL)
+		try:
+		    lines = f.readlines()
+		finally:
+		    f.close()
+		
+		out = []
+		uniqueSet = set()
+
+		if not lines:
+			logger.error ('Error reading from IMSR_STRAIN_URL: %s' % err)
+			logger.error ('No IMSR strain data will be stored')
+
+			raise Exception ('Could not retrieve strain data from IMSR')
+			return out
+			
+		for line in lines:
+			# skip blank lines
+			if len(line.strip()) == 0:
+				continue
+
+			items = line.split('\t')
+
+			# report (and skip) lines with too few fields; this would
+			# indicate a bug in IMSR
+			if len(items) < 14:
+				logger.debug ('Line from IMSR has too few fields (%d): %s' % (len(items), line))
+				continue
+
+			# extract needed data
+			nomenFlag = 0
+			if items[0] == '+':
+				nomenFlag = 1
+			
+			[ imsrID, strain, repository ] = items[1:4]
+			url = items[13]
+			
+			key = (imsrID, strain, repository)
+			if key not in uniqueSet:
+				uniqueSet.add(key)
+				out.append( (strain, nomenFlag, imsrID, repository, url) )
+			
+		logger.debug ('Condensed %d lines down to %d' % (len(lines), len(out)))
+		del lines
+		gc.collect()
+			
+		return out
 
 if __name__=="__main__":
 	imsrDB = IMSRDatabase()
 	imsrDB.IMSR_COUNT_URL = "http://www.findmice.org/report/mgiCounts.txt"
 	imsrDB.IMSR_COUNT_TIMEOUT=300
+	imsrStrains = IMSRStrainData()
 	import unittest
 	class IMSRDBTestCase(unittest.TestCase):
 		def test_queryAllCounts(self):
@@ -85,4 +145,9 @@ if __name__=="__main__":
 			print "count of markers = %d"%len(markerCount)
 			self.assertTrue(markerCount["MGI:97490"] > 0,"Pax6 count not greater than zero")
 			print "pax6 count = %s"%markerCount["MGI:97490"]
+	class IMSRStrainTestCase(unittest.TestCase):
+		def test_queryStrains(self):
+			lines = imsrStrains.queryStrains()
+			self.assertTrue(len(lines) > 0, 'Failed to retrieve lines')
+			print 'Got %d strain lines' % len(lines)
 	unittest.main()
