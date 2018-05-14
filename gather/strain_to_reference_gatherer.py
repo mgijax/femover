2a14003a519a24e935e6e7257d81e979dc0c2d6d
Jonathan Beal <jsb@jax.org>
2018-05-14 12:18:49 -0400

    updated strain_to_reference_gatherer.py to bring in extra references for each strain

--- gather/strain_to_reference_gatherer.py
+++ gather/strain_to_reference_gatherer.py
@@ -15,12 +15,12 @@ class StrainToReferenceGatherer (Gatherer.Gatherer):
 	#	collates results, writes tab-delimited text file
 
 	def collateResults(self):
-		self.finalColumns = [ '_Object_key', '_Refs_key', 'qualifier' ]
+		self.finalColumns = [ '_Strain_key', '_Refs_key', 'qualifier' ]
 		self.finalResults = []
 		
-		cols, rows = self.results[0]
+		cols, rows = self.results[-1]
 		
-		strainCol = Gatherer.columnNumber(cols, '_Object_key')
+		strainCol = Gatherer.columnNumber(cols, '_Strain_key')
 		refsCol = Gatherer.columnNumber(cols, '_Refs_key')
 		
 		lastStrain = None
@@ -45,18 +45,81 @@ class StrainToReferenceGatherer (Gatherer.Gatherer):
 ###--- globals ---###
 
 cmds = [
-	'''select mra._Object_key, r.year, c.numericPart, mra._Refs_key
-		from mgi_reference_assoc mra, bib_citation_cache c, bib_refs r, %s t
+	# 0. collect (in a temp table) the list of references for each strain.  Largely adapted from
+	#	PRB_getStrainReferences() stored procedure in pgmgddbschema product.
+	'''select v._Strain_key, e._Refs_key
+		into temp table strainReferences
+		from mld_expts e, mld_insitu m, %s v
+		where e._Expt_key = m._Expt_key
+			and m._Strain_key = v._Strain_key
+		union
+		select v._Strain_key, e._Refs_key
+		from mld_expts e, mld_fish m, %s v
+		where e._Expt_key = m._Expt_key
+			and m._Strain_key = v._Strain_key
+		union
+		select v._Strain_key, e._Refs_key
+		from mld_expts e, mld_matrix m, crs_cross c, %s v
+		where e._Expt_key = m._Expt_key
+			and m._Cross_key = c._Cross_key
+			and c._femaleStrain_key = v._Strain_key
+		union
+		select v._Strain_key, e._Refs_key
+		from mld_expts e, mld_matrix m, crs_cross c, %s v
+		where e._Expt_key = m._Expt_key
+			and m._Cross_key = c._Cross_key
+			and c._maleStrain_key = v._Strain_key
+		union
+		select v._Strain_key, e._Refs_key
+		from mld_expts e, mld_matrix m, crs_cross c, %s v
+		where e._Expt_key = m._Expt_key
+			and m._Cross_key = c._Cross_key
+			and c._StrainHO_key = v._Strain_key
+		union
+		select v._Strain_key, e._Refs_key
+		from mld_expts e, mld_matrix m, crs_cross c, %s v
+		where e._Expt_key = m._Expt_key
+			and m._Cross_key = c._Cross_key
+			and c._StrainHT_key = v._Strain_key
+		union
+		select v._Strain_key, x._Refs_key
+		from gxd_genotype s, gxd_expression x, %s v
+		where s._Strain_key = v._Strain_key
+			and s._Genotype_key = x._Genotype_key
+		union
+		select t._Strain_key, r._Refs_key
+		from PRB_Reference r, prb_rflv v, prb_allele a, prb_allele_strain s, %s t
+		where r._Reference_key = v._Reference_key
+			and v._RFLV_key = a._RFLV_key
+			and a._Allele_key = s._Allele_key
+			and s._Strain_key = t._Strain_key
+		union
+		select v._Strain_key, r._Refs_key
+		from all_allele a, mgi_reference_assoc r, %s v
+		where a._Allele_key = r._Object_key
+			and r._MGIType_key = 11
+			and a._Strain_key = v._Strain_key
+		union
+		select mra._Object_key, mra._Refs_key
+		from mgi_reference_assoc mra, %s t
 		where mra._RefAssocType_key in (1009, 1010)
-			and mra._Refs_key = c._Refs_key
-			and mra._Refs_key = r._Refs_key
-			and mra._Object_key = t._Strain_key
-		order by 1, 2, 3''' % StrainUtils.getStrainTempTable()
+			and mra._Object_key = t._Strain_key'''.replace('%s', StrainUtils.getStrainTempTable()),
+
+	# 1-2. index the temp table
+	'create index strainIndex1 on strainReferences (_Strain_key)',
+	'create index strainIndex2 on strainReferences (_Refs_key)',
+	
+	# 3. actually pull out the ordered references for each strain
+	'''select distinct sr._Strain_key, r.year, c.numericPart, sr._Refs_key
+		from strainReferences sr, bib_citation_cache c, bib_refs r
+		where sr._Refs_key = c._Refs_key
+			and sr._Refs_key = r._Refs_key
+		order by 1, 2, 3'''
 	]
 
 # order of fields (from the query results) to be written to the
 # output file
-fieldOrder = [ Gatherer.AUTO, '_Object_key', '_Refs_key', 'qualifier' ]
+fieldOrder = [ Gatherer.AUTO, '_Strain_key', '_Refs_key', 'qualifier' ]
 
 # prefix for the filename of the output file
 filenamePrefix = 'strain_to_reference'
