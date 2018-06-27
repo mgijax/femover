4917a34c91859cc813503239d69676176bd166c2
Jonathan Beal <jsb@jax.org>
2018-06-27 13:19:46 -0400

    added (in KeyGenerator.py) abilities to retrieve the data elements that have keys and to keep bidirectional relationships

--- lib/python/KeyGenerator.py
+++ lib/python/KeyGenerator.py
@@ -5,12 +5,16 @@
 import gc
 
 class KeyGenerator:
-	def __init__ (self, tableName = None):
+	def __init__ (self, tableName = None, bidirectional = False):
 		# constructor
 
 		self.tableName = tableName  # name of the table
 		self.dataToKey = {}	    # maps data tuples to their keys
 		self.maxKey = 0		    # max key assigned so far
+		self.keyToData = {}		# maps from key back to its data
+		
+		# do we want to be able to map from a key back to its data?
+		self.bidirectional = bidirectional
 		return
 
 	def nextKey (self):
@@ -26,8 +30,30 @@ class KeyGenerator:
 
 		if not self.dataToKey.has_key(dataTuple):
 			self.dataToKey[dataTuple] = self.nextKey()
+			if self.bidirectional:
+				self.keyToData[self.dataToKey[dataTuple]] = dataTuple
+
 		return self.dataToKey[dataTuple]
 
+	def getDataValue (self, key):
+		# retrieve the data value that is associated with the 'key', or None if 
+		# either the 'key' is invalid or if this object was not instantiated with
+		# the 'bidirectional' flag set
+		
+		if key in self.keyToData:
+			return self.keyToData[key]
+		return None
+	
+	def getDataValues (self):
+		# get the data values that currently have assigned keys
+
+		return self.dataToKey.keys()
+	
+	def __len__ (self):
+		# number of values assigned
+		
+		return len(self.dataToKey)
+	
 	def getTableName (self):
 		# Retrieve the table name for this KeyGenerator.
 
