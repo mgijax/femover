4dbbbab8a30fe6943ef638e2312e27f006525976
Jonathan Beal <jsb@jax.org>
2018-08-07 22:09:46 -0400

    substantial rewrite to move processing workload off the db server and into this script, as this script was really hindering performance when run in parallel with other gatherers

--- gather/strain_snp_gatherer.py
+++ gather/strain_snp_gatherer.py
@@ -8,6 +8,7 @@ import dbAgnostic
 import StrainUtils
 import symbolsort
 import KeyGenerator
+import gc
 
 ###--- Globals ---###
 
@@ -33,7 +34,7 @@ def compareRows(a, b):
 	if c == 0:
 		c = cmp(CHROMOSOME_SEQ_NUM[a[2]], CHROMOSOME_SEQ_NUM[b[2]])
 	return c 
-	
+
 ###--- Classes ---###
 
 class StrainSnpGatherer (Gatherer.CachingMultiFileGatherer):
@@ -71,7 +72,7 @@ class StrainSnpGatherer (Gatherer.CachingMultiFileGatherer):
 			STRAIN_NAME[row[keyCol]] = row[nameCol]
 			STRAIN_ID[row[keyCol]] = row[idCol]
 
-		logger.debug('Collected %d strains' % len(STRAIN_SEQ_NUM))
+		logger.debug('Collected %d strains' % len(toSort))
 		
 		toSort.sort(lambda x, y: symbolsort.nomenCompare(x[0], y[0]))
 		for (name, key) in toSort:
@@ -80,33 +81,10 @@ class StrainSnpGatherer (Gatherer.CachingMultiFileGatherer):
 		logger.debug('Sorted %d strains' % len(toSort))
 		return
 		
-	def fillMissingRows(self, refRows):
-		# fill in any missing cells (since we only have data for non-zero counts).  Assumes that all
-		# rows given are for the same reference strain.
-		
-		# seen[comparison strain key] = set of chromosomes seen so far
-		seen = {}
-
-		for [ refStrainKey, cmpStrainKey, chromosome, allCount, sameCount, diffCount ] in refRows:
-			if cmpStrainKey not in seen:
-				seen[cmpStrainKey] = set()
-			seen[cmpStrainKey].add(chromosome)
-			
-		ct = 0
-		for cmpStrainKey in seen.keys():
-			for chromosome in CHROMOSOME_SEQ_NUM.keys():
-				if chromosome not in seen[cmpStrainKey]:
-					refRows.append( [ refStrainKey, cmpStrainKey, chromosome, 0, 0, 0 ] )
-					ct = ct + 1
-					
-		logger.debug(' - added %d missing cells (zero counts)' % ct) 
-		return refRows
-	
 	def produceOutputRows(self, refRows):
 		# take the list of rows for the current reference strain and produce output rows for the
 		# data files being written
 		
-		refRows = self.fillMissingRows(refRows)
 		refRows.sort(compareRows)
 		logger.debug(' - sorted %d rows' % len(refRows))
 
@@ -134,107 +112,163 @@ class StrainSnpGatherer (Gatherer.CachingMultiFileGatherer):
 		logger.debug(' - wrote %d cells in %d rows' % (cellCt, rowCt))
 		return
 
-	def collateSNPs(self, referenceStrain, comparisonStrains, cacheByStrain):
-		# Query the database for counts for the given reference strain with the list of comparison strains.
-		# Include any data already cached in 'cacheByStrain' from previous calls.  Need to collect data,
-		# generate rows for both database tables, and produce sequence numbers for those rows.  Also need to
-		# collect the inverse data (where each comparison strain is treated as a reference strain and the
-		# reference strain as the comparison strain) so we can process it later on.  This saves extra work
-		# at the database level to compute the came counts with the strains swapped.
-		
-		logger.debug('Processing strain %d: %d cmp strains' % (referenceStrain, len(comparisonStrains)))
+	def getData(self, fromKey, toKey):
+		# get a batch of data from the database and collect two sets of data from it (chromosomes and snps)
 
-		# list of rows being produced for the reference strain, each row being:
-		#	[ ref strain key, cmp strain key, chromosome, all count, same count, diff count ]
-		refRows = []
+		# The strain on the database server from doing joins of strain alleles to strain alleles was
+		# significantly impacting performance, as this gather ran in about 52 minutes solo, but 4.25
+		# hours when run in parallel with other gatherers.  So, we're doing computations in python
+		# and trying to minimize the load on the db server.
 		
-		# pull in any cached rows from previous searches, where this reference strain was previously
-		# used as a comparison strain
-		if referenceStrain in cacheByStrain:
-			refRows = cacheByStrain[referenceStrain]
-			del cacheByStrain[referenceStrain]
-			logger.debug(' - got %d rows from cache' % len(refRows))
+		cmd = '''select ref._mgdStrain_key as ref_strain_key,
+				ref._ConsensusSNP_key, cc.chromosome, ref.allele
+			from snp_consensussnp_strainallele ref, snp_coord_cache cc
+			where ref._ConsensusSNP_key = cc._ConsensusSNP_key
+				and cc._VarClass_key = 1878510
+				and ref.allele in ('A', 'C', 'G', 'T', '?')
+				and cc.isMultiCoord = 0
+				and exists (select 1 from %s t where ref._mgdStrain_key = t._Strain_key) 
+				and ref._ConsensusSNP_key >= %d
+				and ref._ConsensusSNP_key < %d''' % (StrainUtils.getStrainTempTable(), fromKey, toKey)
+				
+		cols, rows = dbAgnostic.execute(cmd)	
+		logger.debug(' - got data (%d rows)' % len(rows))
 		
-		# if we have any comparison strains left, then we need to get new records from the database
-		if comparisonStrains:
-			# need to retrieve allele calls to compare for the same consensus SNP, with the given reference
-			# strain and any of the set of comparison strains.  Note that we skip multi-coordinate SNPs, as
-			# these are omitted from public display.  Also, when computing counts:
-			#	1. in the 'all' count, include any allele calls of:  ACGT?
-			#	2. for 'same' or 'different', include only allele calls:  ACGT (exclude ?)
-			#	3. all other allele calls are suppressed from public summaries, so exclude them from the counts
-			# Only deal with SNPs of variation class "SNP", omitting Mixed, MNP, and IN-DEL, as those don't
-			# appear on the public summary page.
+		strainCol = Gatherer.columnNumber(cols, 'ref_strain_key')
+		snpCol = Gatherer.columnNumber(cols, '_ConsensusSnp_key')
+		chromCol = Gatherer.columnNumber(cols, 'chromosome')
+		alleleCol = Gatherer.columnNumber(cols, 'allele')
+		
+		chromosomes = {}		# consensus SNP key : chromosome
+		snps = {}				# consensus SNP key : { strain key : allele } }
+		
+		for row in rows:
+			snpKey = row[snpCol]
+			chromosomes[snpKey] = row[chromCol]
 			
-			cmd = '''select ref._mgdStrain_key as ref_strain_key,
-					cmp._mgdStrain_key as cmp_strain_key,
-					cc.chromosome,
-					count(1) as all_count,
-					sum(case when (ref.allele = cmp.allele and ref.allele != '?')
-						then 1 else 0 end) as same_count,
-					sum(case when (ref.allele != cmp.allele and ref.allele != '?' and cmp.allele != '?')
-						then 1 else 0 end) as diff_count
-				from snp_consensussnp_strainallele ref, snp_consensussnp_strainallele cmp,
-					snp_coord_cache cc
-				where ref._ConsensusSNP_key = cmp._ConsensusSNP_key
-					and ref._mgdStrain_key = %d
-					and cmp._mgdStrain_key in (%s)
-					and cmp._ConsensusSNP_key = cc._ConsensusSNP_key
-					and ref.allele in ('A', 'C', 'G', 'T', '?')
-					and cmp.allele in ('A', 'C', 'G', 'T', '?')
-					and cc.isMultiCoord = 0
-					and cc._VarClass_key = 1878510
-				group by 1, 2, 3'''
-		
-			cmpStrains = ','.join(map(str, comparisonStrains))
-			cols, rows = dbAgnostic.execute(cmd % (referenceStrain, cmpStrains))
-			logger.debug(' - got %d rows from db' % len(rows))
-		
-			refStrainCol = Gatherer.columnNumber(cols, 'ref_strain_key')
-			cmpStrainCol = Gatherer.columnNumber(cols, 'cmp_strain_key')
-			chromosomeCol = Gatherer.columnNumber(cols, 'chromosome')
-			allCountCol = Gatherer.columnNumber(cols, 'all_count')
-			sameCountCol = Gatherer.columnNumber(cols, 'same_count')
-			diffCountCol = Gatherer.columnNumber(cols, 'diff_count')
-
-			for row in rows:
-				refStrainKey = row[refStrainCol]
-				cmpStrainKey = row[cmpStrainCol]
-				chromosome = row[chromosomeCol]
-				allCount = row[allCountCol]
-				sameCount = row[sameCountCol]
-				diffCount = row[diffCountCol]
-
-				# collect the row for the reference strain
-				refRow = [ refStrainKey, cmpStrainKey, chromosome, allCount, sameCount, diffCount ]
-				refRows.append(refRow)
-
-				# cache the same data, but with the strains flipped, for when we process the comparison
-				# strain as a reference strain
-				cmpRow = [ cmpStrainKey, refStrainKey, chromosome, allCount, sameCount, diffCount ]
-				if cmpStrainKey not in cacheByStrain:
-					cacheByStrain[cmpStrainKey] = []
-				cacheByStrain[cmpStrainKey].append(cmpRow)
+			if snpKey not in snps:
+				snps[snpKey] = {}
+			snps[snpKey][row[strainCol]] = row[alleleCol]
 			
-		self.produceOutputRows(refRows)
+		del rows
+		del cols
+		gc.collect()
+
+		logger.debug(' - collated into %d SNPs' % len(snps))
+		return chromosomes, snps
+
+	def addToCounts(self, chromosomes, snps):
+		# walk through the SNPs and their chromosomes, adding to the heat map counts as needed
+		
+		for snpKey in snps.keys():
+			alleles = snps[snpKey]
+			strains = alleles.keys()
+			chromosome = chromosomes[snpKey]
+			
+			for refStrain in strains:
+				refAllele = alleles[refStrain]
+				for cmpStrain in strains:
+					if refStrain != cmpStrain:
+						# using a variable to try to save two hash lookups and speed up slightly
+						ch = self.all[refStrain][cmpStrain]
+						ch[chromosome] = ch[chromosome] + 1
+
+						cmpAllele = alleles[cmpStrain]
+						
+						if (refAllele != '?'):
+							if (refAllele == cmpAllele):
+								# using a variable to try to save two hash lookups and speed up slightly
+								ch = self.same[refStrain][cmpStrain]
+								ch[chromosome] = ch[chromosome] + 1
+							elif (cmpAllele != '?'):
+								# using a variable to try to save two hash lookups and speed up slightly
+								ch = self.diff[refStrain][cmpStrain]
+								ch[chromosome] = ch[chromosome] + 1
+			
+		logger.debug(' - added to counts')
+		return
+	
+	def produceRows(self, refStrain):
+		# take the collections of snp counts and produce output rows for them
+		
+		chromosomes = CHROMOSOME_SEQ_NUM.keys()
+		out = []
+
+		for cmpStrain in self.all[refStrain].keys():
+			for chromosome in chromosomes:
+				out.append([
+					refStrain, cmpStrain, chromosome,
+					self.all[refStrain][cmpStrain][chromosome],
+					self.same[refStrain][cmpStrain][chromosome],
+					self.diff[refStrain][cmpStrain][chromosome]
+				])
+
+		logger.debug(' - produced %d rows for %d' % (len(out), refStrain))
+		return out			
+
+	def processBatch(self, fromKey, toKey):
+		# process a batch of consensus SNPs from 'fromKey' (inclusive) up to 'toKey' (exclusive)
+		
+		logger.debug('Working on %d-%d' % (fromKey, toKey))
+
+		chromosomes, snps = self.getData(fromKey, toKey)
+		self.addToCounts(chromosomes, snps)
+		
+		del chromosomes
+		del snps
+		gc.collect()
+		return
+				
+	def initialize(self):
+		# initialize the snp count collections self.all, self.same, and self.diff
+		
+		self.all = {}		# ref strain : { cmp strain : { chromosome : count of all snps } }
+		self.same = {}		# ref strain : { cmp strain : { chromosome : count of snps with same allele calls } }
+		self.diff = {}		# ref strain : { cmp strain : { chromosome : count of snps with different allele calls } }
+
+		for refStrain in STRAIN_SEQ_NUM:
+			self.all[refStrain] = {}
+			self.same[refStrain] = {}
+			self.diff[refStrain] = {}
+
+			for cmpStrain in STRAIN_SEQ_NUM:
+				if refStrain != cmpStrain:
+					self.all[refStrain][cmpStrain] = {}
+					self.same[refStrain][cmpStrain] = {}
+					self.diff[refStrain][cmpStrain] = {}
+
+					for chromosome in CHROMOSOME_SEQ_NUM:
+						self.all[refStrain][cmpStrain][chromosome] = 0
+						self.same[refStrain][cmpStrain][chromosome] = 0
+						self.diff[refStrain][cmpStrain][chromosome] = 0
+
+		logger.debug('Initialized collections')
 		return
 	
 	def collateResults(self):
 		self.collateChromosomes()
 		self.collateStrains()
 
-		# cacheByStrain[strain key] = [ list of rows discovered and cached while processing this strain as a comparison strain for another reference strain ]
-		# This cache will be built as each reference strain is processed and will be decreased as each strain with cached
-		# data itself is processed as a reference strain.
-		cacheByStrain = {}
+		maxSnpKey = int(self.results[-1][1][0][0])
+		logger.debug('Found max consensus SNP key = %d' % maxSnpKey)
 
-		toDo = STRAIN_SEQ_NUM.keys()
-		while len(toDo) > 0:
-			referenceStrain = toDo[0]
-			toDo = toDo[1:]
+		self.initialize()
+
+		chunkSize = 175000
+		startKey = 1
+		while startKey <= maxSnpKey:
+			endKey = startKey + chunkSize
+			self.processBatch(startKey, endKey)
+			startKey = endKey
 			
-			self.collateSNPs(referenceStrain, toDo, cacheByStrain)
+		logger.debug('Producing output')
 
+		strainKeys = self.all.keys()
+		strainKeys.sort()
+		
+		for refStrain in strainKeys:
+			self.produceOutputRows(self.produceRows(refStrain))
+			
 		logger.debug('Finished processing strains')
 		return
 
@@ -242,26 +276,26 @@ class StrainSnpGatherer (Gatherer.CachingMultiFileGatherer):
 
 cmds = [
 	# 0. get unique set of chromosomes having SNPs
-	'''with chromosomes as (select distinct chromosome
-			from snp_coord_cache)
-		select c.chromosome, c.sequenceNum
-		from mrk_chromosome c, chromosomes s
+	'''select c.chromosome, c.sequenceNum
+		from mrk_chromosome c
 		where c._Organism_key = 1
-			and s.chromosome = c.chromosome
+			and exists (select 1 from snp_coord_cache cc where c.chromosome = cc.chromosome)
 		order by c.sequenceNum''',
 	
 	# 1. get unique set of strains with SNPs
-	'''with strains as (
-			select distinct _mgdStrain_key from snp_consensussnp_strainallele)
-		select p._Strain_key, p.strain, a.accID
-		from prb_strain p, strains s, %s t, acc_accession a
-		where p._Strain_key = s._mgdStrain_key
-		and p._Strain_key = t._Strain_key
+	'''select p._Strain_key, p.strain, a.accID
+		from prb_strain p, %s t, acc_accession a
+		where p._Strain_key = t._Strain_key
 		and p._Strain_key = a._Object_key
 		and a._MGIType_key = 10
 		and a._LogicalDB_key = 1
 		and a.preferred = 1
+		and exists (select 1 from snp_consensussnp_strainallele s
+			where t._Strain_key = s._mgdStrain_key)
 		order by p.strain''' % StrainUtils.getStrainTempTable(),
+		
+	# 2. get the max consensus SNP key
+	'select max(_ConsensusSnp_key) as maxKey from snp_consensussnp',
 	]
 
 # definitions for output files to be produced
