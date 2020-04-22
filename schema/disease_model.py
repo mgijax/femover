db78cbbbac112c210d53bc8c9b7dc635e541fed4
jsb <jsb@jax.org>
2020-04-22 15:48:45 -0400

    updated disease detail gatherer and associated tables

--- schema/disease_model.py
+++ schema/disease_model.py
@@ -1,4 +1,4 @@
-#!/usr/local/bin/python
+#!./python
 
 import Table
 
@@ -14,20 +14,20 @@ tableName = 'disease_model'
 
 # MySQL statement to create this table
 createStatement = '''CREATE TABLE %s  ( 
-	disease_model_key	int		not null,
-	genotype_key		int		not null,
-	disease			text	not null,
-	disease_id		text	not null,
-	is_not_model		int		not null,
-	PRIMARY KEY(disease_model_key))''' % tableName
+        disease_model_key       int             not null,
+        genotype_key            int             not null,
+        disease                 text    not null,
+        disease_id              text    not null,
+        is_not_model            int             not null,
+        PRIMARY KEY(disease_model_key))''' % tableName
 
 # Maps from index suffix to create statement for that index.  In each
 # statement, the first %s is for the index name, and the second is for the
 # table name.
 indexes = {
-	'disease_id' : 'create index %s on %s (disease_id)',
-	'genotype_key' : 'create index %s on %s (genotype_key)',
-	}
+        'disease_id' : 'create index %s on %s (disease_id)',
+        'genotype_key' : 'create index %s on %s (genotype_key)',
+        }
 
 # column name -> (related table, column in related table)
 keys = { 'genotype_key' : ('genotype', 'genotype_key') }
@@ -37,27 +37,27 @@ clusteredIndex = None
 
 # comments describing the table, columns, and indexes
 comments = {
-	Table.TABLE : 'defines a mouse model for a human disease',
-	Table.COLUMN : {
-		'disease_model_key' : 'unique key that identifies a mouse model for a human disease',
-		'genotype_key' : 'foreign key to ',
-		'disease' : 'name of the disease, cached here for convenience',
-		'disease_id' : 'primary ID for the disease, cached here for convenience',
-		'is_not_model' : 'flag to indicate if this genotype was expected to be a model for the human disease but was not (1), or if it actually does model the disease (0)',
-		},
-	Table.INDEX : {
-		'disease_id' : 'provides quick access to models for a disease by its ID, for convenience in SQL access',
-		'genotype_key' : 'provides access to diseases for which a given genotype is a mouse model',
-		},
-	}
+        Table.TABLE : 'defines a mouse model for a human disease',
+        Table.COLUMN : {
+                'disease_model_key' : 'unique key that identifies a mouse model for a human disease',
+                'genotype_key' : 'foreign key to ',
+                'disease' : 'name of the disease, cached here for convenience',
+                'disease_id' : 'primary ID for the disease, cached here for convenience',
+                'is_not_model' : 'flag to indicate if this genotype was expected to be a model for the human disease but was not (1), or if it actually does model the disease (0)',
+                },
+        Table.INDEX : {
+                'disease_id' : 'provides quick access to models for a disease by its ID, for convenience in SQL access',
+                'genotype_key' : 'provides access to diseases for which a given genotype is a mouse model',
+                },
+        }
 
 # global instance of this Table object
 table = Table.Table (tableName, createStatement, indexes, keys, comments,
-		clusteredIndex)
+                clusteredIndex)
 
 ###--- Main program ---###
 
 # if executed as a script, pass the global Table object into the general
 # main program for Table subclasses
 if __name__ == '__main__':
-	Table.main(table)
+        Table.main(table)
--- schema/disease_synonym.py
+++ schema/disease_synonym.py
@@ -1,4 +1,4 @@
-#!/usr/local/bin/python
+#!./python
 
 import Table
 
@@ -14,18 +14,18 @@ tableName = 'disease_synonym'
 
 # MySQL statement to create this table
 createStatement = '''CREATE TABLE %s  ( 
-	unique_key	int		not null,
-	disease_key	int		not null,
-	synonym		text	not null,
-	sequence_num	int		not null,
-	PRIMARY KEY(unique_key))''' % tableName
+        unique_key      int             not null,
+        disease_key     int             not null,
+        synonym         text    not null,
+        sequence_num    int             not null,
+        PRIMARY KEY(unique_key))''' % tableName
 
 # Maps from index suffix to create statement for that index.  In each
 # statement, the first %s is for the index name, and the second is for the
 # table name.
 indexes = {
-	'synonym' : 'create index %s on %s (synonym)',
-	}
+        'synonym' : 'create index %s on %s (synonym)',
+        }
 
 # column name -> (related table, column in related table)
 keys = { 'disease_key' : ('disease', 'disease_key') }
@@ -35,26 +35,26 @@ clusteredIndex = ('disease_key', 'create index %s on %s (disease_key)')
 
 # comments describing the table, columns, and indexes
 comments = {
-	Table.TABLE : 'contains synonyms for diseases',
-	Table.COLUMN : {
-		'unique_key' : 'uniquely identifies this synonym/disease pair',
-		'disease_key' : 'foreign key to disease table',
-		'synonym' : 'the synonym itself',
-		'sequence_num' : 'used to order synonyms for each disease',
-		},
-	Table.INDEX : {
-		'synonym' : 'case-sensitive searching by synonym',
-		'disease_key' : 'clustered index to keep synonyms for each disease together',
-		},
-	}
+        Table.TABLE : 'contains synonyms for diseases',
+        Table.COLUMN : {
+                'unique_key' : 'uniquely identifies this synonym/disease pair',
+                'disease_key' : 'foreign key to disease table',
+                'synonym' : 'the synonym itself',
+                'sequence_num' : 'used to order synonyms for each disease',
+                },
+        Table.INDEX : {
+                'synonym' : 'case-sensitive searching by synonym',
+                'disease_key' : 'clustered index to keep synonyms for each disease together',
+                },
+        }
 
 # global instance of this Table object
 table = Table.Table (tableName, createStatement, indexes, keys, comments,
-		clusteredIndex)
+                clusteredIndex)
 
 ###--- Main program ---###
 
 # if executed as a script, pass the global Table object into the general
 # main program for Table subclasses
 if __name__ == '__main__':
-	Table.main(table)
+        Table.main(table)
