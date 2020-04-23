6f1c11b52eb154637f394ead19dcab29e22ad24b
jsb <jsb@jax.org>
2020-04-23 13:19:55 -0400

    updated statistic and glossary gatherers and tables to Python 3.7

--- schema/glossary_term.py
+++ schema/glossary_term.py
@@ -1,4 +1,4 @@
-#!/usr/local/bin/python
+#!./python
 
 import Table
 
@@ -12,42 +12,42 @@ tableName = 'glossary_term'
 
 # PgSQL statement to create this table
 createStatement = '''CREATE TABLE %s  ( 
-	unique_key		int		not null,
-	glossary_key			text		not null,
-	display_name			text		not null,
-	definition		text		not null,
-	PRIMARY KEY(unique_key))''' % tableName
+        unique_key              int             not null,
+        glossary_key                    text            not null,
+        display_name                    text            not null,
+        definition              text            not null,
+        PRIMARY KEY(unique_key))''' % tableName
 
 # Maps from index suffix to create statement for that index.  In each
 # statement, the first %s is for the index name, and the second is for the
 # table name.
 indexes = {
-	'glossary_key' : 'create index %s on %s (glossary_key)',
-	}
+        'glossary_key' : 'create index %s on %s (glossary_key)',
+        }
 keys = {}
 
 
 clusteredIndex=None
 # comments describing the table, columns, and indexes
 comments = {
-	Table.TABLE : 'contains data from the glossary.rcd file',
-	Table.COLUMN : {
-		'unique_key' : 'unique identifier for this table',
-		'glossary_key' : 'readable key for linking',
-		'display_name' : 'glossary term display value',
-		'definition' : 'glossary term definition',
-		},
-	Table.INDEX : {
-		},
-	}
+        Table.TABLE : 'contains data from the glossary.rcd file',
+        Table.COLUMN : {
+                'unique_key' : 'unique identifier for this table',
+                'glossary_key' : 'readable key for linking',
+                'display_name' : 'glossary term display value',
+                'definition' : 'glossary term definition',
+                },
+        Table.INDEX : {
+                },
+        }
 
 # global instance of this Table object
 table = Table.Table (tableName, createStatement, indexes, keys, comments,
-		clusteredIndex)
+                clusteredIndex)
 
 ###--- Main program ---###
 
 # if executed as a script, pass the global Table object into the general
 # main program for Table subclasses
 if __name__ == '__main__':
-	Table.main(table)
+        Table.main(table)
--- schema/statistic.py
+++ schema/statistic.py
@@ -1,4 +1,4 @@
-#!/usr/local/bin/python
+#!./python
 
 import Table
 
@@ -14,56 +14,56 @@ tableName = 'statistic'
 
 # MySQL statement to create this table
 createStatement = '''CREATE TABLE %s  ( 
-	unique_key		int		not null,
-	statistic_key		int		not null,
-	name			text		not null,
-	tooltip			text	null,
-	value			int			not null,
-	group_name			text		not null,
-	sequencenum			int		not null,
-	group_sequencenum		int not	null,
-	PRIMARY KEY(unique_key))''' % tableName
+        unique_key              int             not null,
+        statistic_key           int             not null,
+        name                    text            not null,
+        tooltip                 text    null,
+        value                   int                     not null,
+        group_name                      text            not null,
+        sequencenum                     int             not null,
+        group_sequencenum               int not null,
+        PRIMARY KEY(unique_key))''' % tableName
 
 # Maps from index suffix to create statement for that index.  In each
 # statement, the first %s is for the index name, and the second is for the
 # table name.
 indexes = {
-	'statistic_key' : 'create index %s on %s (statistic_key)',
-	'name' : 'create index %s on %s (name)',
-	'tooltip' : 'create index %s on %s (tooltip)',
-	'group_name' : 'create index %s on %s (group_name)'
-	}
+        'statistic_key' : 'create index %s on %s (statistic_key)',
+        'name' : 'create index %s on %s (name)',
+        'tooltip' : 'create index %s on %s (tooltip)',
+        'group_name' : 'create index %s on %s (group_name)'
+        }
 
 keys = {
-	}
+        }
 
 # index used to cluster data in the table
 clusteredIndex = None
 
 # comments describing the table, columns, and indexes
 comments = {
-	Table.TABLE : 'contains all the most recent data measurements',
-	Table.COLUMN : {
-		'unique_key' : 'unique key for this row',
-		'statistic_key' : 'unique key for this statistic',
-		'name' : 'long name of statistic',
-		'tooltip' : 'mouse-over tooltip for the statistic',
-		'value' : 'integer value measured for this statistic',
-		'group_name' : 'what group is this statistic part of',
-		'sequencenum' : 'Ordering of this statistic',
-		'group_sequencenum' : 'Ordering of the group',
-		},
-	Table.INDEX : {
-		},
-	}
+        Table.TABLE : 'contains all the most recent data measurements',
+        Table.COLUMN : {
+                'unique_key' : 'unique key for this row',
+                'statistic_key' : 'unique key for this statistic',
+                'name' : 'long name of statistic',
+                'tooltip' : 'mouse-over tooltip for the statistic',
+                'value' : 'integer value measured for this statistic',
+                'group_name' : 'what group is this statistic part of',
+                'sequencenum' : 'Ordering of this statistic',
+                'group_sequencenum' : 'Ordering of the group',
+                },
+        Table.INDEX : {
+                },
+        }
 
 # global instance of this Table object
 table = Table.Table (tableName, createStatement, indexes, keys, comments,
-		clusteredIndex)
+                clusteredIndex)
 
 ###--- Main program ---###
 
 # if executed as a script, pass the global Table object into the general
 # main program for Table subclasses
 if __name__ == '__main__':
-	Table.main(table)
+        Table.main(table)
