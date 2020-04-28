b8281be5db88a84d82d35557e59f8d59716f0199
jsb <jsb@jax.org>
2020-04-28 13:46:16 -0400

    updated antibody and antigen tables and gatherers

--- schema/antibody_to_reference.py
+++ schema/antibody_to_reference.py
@@ -1,4 +1,4 @@
-#!/usr/local/bin/python
+#!./python
 
 import Table
 
@@ -11,49 +11,49 @@ tableName = 'antibody_to_reference'
 
 # MySQL statement to create this table
 createStatement = '''CREATE TABLE %s  ( 
-	unique_key	int	NOT NULL,
-	antibody_key	int	NOT NULL,
-	reference_key	int	NOT NULL,
-	qualifier	text	NULL,
-	PRIMARY KEY(unique_key))''' % tableName
+        unique_key      int     NOT NULL,
+        antibody_key    int     NOT NULL,
+        reference_key   int     NOT NULL,
+        qualifier       text    NULL,
+        PRIMARY KEY(unique_key))''' % tableName
 
 # Maps from index suffix to create statement for that index.  In each
 # statement, the first %s is for the index name, and the second is for the
 # table name.
 indexes = {
-	'reference_key' : 'create index %s on %s (reference_key)',
-	}
+        'reference_key' : 'create index %s on %s (reference_key)',
+        }
 
 keys = {
-	'antibody_key' : ('antibody', 'antibody_key'),
-	'reference_key' : ('reference', 'reference_key'),
-	}
+        'antibody_key' : ('antibody', 'antibody_key'),
+        'reference_key' : ('reference', 'reference_key'),
+        }
 
 # index used to cluster data in the table
 clusteredIndex = ('antibody_key', 'create index %s on %s (antibody_key)')
 
 # comments describing the table, columns, and indexes
 comments = {
-	Table.TABLE : 'join table between the antibody and reference flowers',
-	Table.COLUMN : {
-		'unique_key' : 'unique identifier for this record, no other purpose',
-		'antibody_key' : 'identifies the antibody',
-		'reference_key' : 'identifies the reference',
-		'qualifier' : 'qualifier describing the association (often null)',
-		},
-	Table.INDEX : {
-		'reference_key' : 'look up all antibodies for a reference',
-		'antibody_key' : 'clusters data so all references for an antibody are stored together on disk, to aid quick retrieval',
-		},
-	}
+        Table.TABLE : 'join table between the antibody and reference flowers',
+        Table.COLUMN : {
+                'unique_key' : 'unique identifier for this record, no other purpose',
+                'antibody_key' : 'identifies the antibody',
+                'reference_key' : 'identifies the reference',
+                'qualifier' : 'qualifier describing the association (often null)',
+                },
+        Table.INDEX : {
+                'reference_key' : 'look up all antibodies for a reference',
+                'antibody_key' : 'clusters data so all references for an antibody are stored together on disk, to aid quick retrieval',
+                },
+        }
 
 # global instance of this Table object
 table = Table.Table (tableName, createStatement, indexes, keys, comments,
-		clusteredIndex)
+                clusteredIndex)
 
 ###--- Main program ---###
 
 # if executed as a script, pass the global Table object into the general
 # main program for Table subclasses
 if __name__ == '__main__':
-	Table.main(table)
+        Table.main(table)
--- schema/marker_to_antibody.py
+++ schema/marker_to_antibody.py
@@ -1,4 +1,4 @@
-#!/usr/local/bin/python
+#!./python
 
 import Table
 
@@ -11,54 +11,54 @@ tableName = 'marker_to_antibody'
 
 # MySQL statement to create this table
 createStatement = '''CREATE TABLE %s  ( 
-	unique_key	int		NOT NULL,
-	marker_key	int		NOT NULL,
-	antibody_key	int 		NOT NULL,
-	reference_key	int		NULL,
-	qualifier	text	NULL,
-	PRIMARY KEY(unique_key))''' % tableName
+        unique_key      int             NOT NULL,
+        marker_key      int             NOT NULL,
+        antibody_key    int             NOT NULL,
+        reference_key   int             NULL,
+        qualifier       text    NULL,
+        PRIMARY KEY(unique_key))''' % tableName
 
 # Maps from index suffix to create statement for that index.  In each
 # statement, the first %s is for the index name, and the second is for the
 # table name.
 indexes = {
-	'antibody_key' : 'create index %s on %s (antibody_key)',
-	'reference_key' : 'create index %s on %s (reference_key)',
-	}
+        'antibody_key' : 'create index %s on %s (antibody_key)',
+        'reference_key' : 'create index %s on %s (reference_key)',
+        }
 
 keys = {
-	'marker_key' : ('marker', 'marker_key'),
-	'antibody_key' : ('antibody', 'antibody_key'),
-	'reference_key' : ('reference', 'reference_key'),
-	}
+        'marker_key' : ('marker', 'marker_key'),
+        'antibody_key' : ('antibody', 'antibody_key'),
+        'reference_key' : ('reference', 'reference_key'),
+        }
 
 # index used to cluster data in the table
 clusteredIndex = ('marker_key', 'create index %s on %s (marker_key)')
 
 # comments describing the table, columns, and indexes
 comments = {
-	Table.TABLE : 'join table between the marker and antibody flowers',
-	Table.COLUMN : {
-		'unique_key' : 'unique key identifying this association, no other purpose',
-		'marker_key' : 'identifies the marker',
-		'antibody_key' : 'identifies an antibody associated with the marker',
-		'reference_key' : 'identifies the reference for this association (often null)',
-		'qualifier' : 'qualifier describing this association (often null)',
-		},
-	Table.INDEX : {
-		'marker_key' : 'clusters the data so all antibody associations for a marker are stored together on disk, aiding quick retrieval',
-		'antibody_key' : 'look up all marker for an antibody',
-		'reference_key' : 'look up all marker/antibody associations for a reference',
-		},
-	}
+        Table.TABLE : 'join table between the marker and antibody flowers',
+        Table.COLUMN : {
+                'unique_key' : 'unique key identifying this association, no other purpose',
+                'marker_key' : 'identifies the marker',
+                'antibody_key' : 'identifies an antibody associated with the marker',
+                'reference_key' : 'identifies the reference for this association (often null)',
+                'qualifier' : 'qualifier describing this association (often null)',
+                },
+        Table.INDEX : {
+                'marker_key' : 'clusters the data so all antibody associations for a marker are stored together on disk, aiding quick retrieval',
+                'antibody_key' : 'look up all marker for an antibody',
+                'reference_key' : 'look up all marker/antibody associations for a reference',
+                },
+        }
 
 # global instance of this Table object
 table = Table.Table (tableName, createStatement, indexes, keys, comments,
-		clusteredIndex)
+                clusteredIndex)
 
 ###--- Main program ---###
 
 # if executed as a script, pass the global Table object into the general
 # main program for Table subclasses
 if __name__ == '__main__':
-	Table.main(table)
+        Table.main(table)
