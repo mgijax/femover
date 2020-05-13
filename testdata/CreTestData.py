a4df007ecd67ec21af2b705f2a5b89ac1881e4ec
jsb <jsb@jax.org>
2020-05-13 16:41:09 -0400

    test scaffolding conversion to python 3.7

--- testdata/CreTestData.py
+++ testdata/CreTestData.py
@@ -1,4 +1,4 @@
-#!/usr/local/bin/python
+#!./python
 
 # this TestData import brings in the constants used below (E.g. ID, DESCRIPTION,...)
 from TestData import *
@@ -16,34 +16,34 @@ TEMPLATE_CRE_STRUCTURE_SQL = """
                 WHERE EXISTS (SELECT 1 FROM struct s WHERE clo._ancestorobject_key = s._term_key) ),
         closure AS (SELECT * from struct UNION ALL SELECT * FROM child) 
         SELECT count(distinct cre._allele_key) FROM gxd_expression e,gxd_allelegenotype gag,all_cre_cache cre, closure s,tmp_emaps_ad tea
-	WHERE e._emapa_term_key = tea._emapa_term_key
-	    and e._stage_key = tea._stage_key
-	    and tea._emaps_key=s._term_key 
-	    AND e.isrecombinase = 1 
-	    AND gag._genotype_key=e._genotype_key 
-	    AND gag._allele_key=cre._allele_key;
+        WHERE e._emapa_term_key = tea._emapa_term_key
+            and e._stage_key = tea._stage_key
+            and tea._emaps_key=s._term_key 
+            AND e.isrecombinase = 1 
+            AND gag._genotype_key=e._genotype_key 
+            AND gag._allele_key=cre._allele_key;
 """
 # The list of queries to generate GXD test data
 Queries = [
-{	ID:"creDorsalMesocardiumAlleleCount",
-	DESCRIPTION:"Count of cre alleles associated with dorsal mesocardium",
-	SQLSTATEMENT:TEMPLATE_CRE_STRUCTURE_SQL%"dorsal mesocardium"
+{       ID:"creDorsalMesocardiumAlleleCount",
+        DESCRIPTION:"Count of cre alleles associated with dorsal mesocardium",
+        SQLSTATEMENT:TEMPLATE_CRE_STRUCTURE_SQL%"dorsal mesocardium"
 },
-{	ID:"creEsophagusAlleleCount",
-	DESCRIPTION:"Count of cre alleles associated with esophagus",
-	SQLSTATEMENT:TEMPLATE_CRE_STRUCTURE_SQL%"esophagus"
+{       ID:"creEsophagusAlleleCount",
+        DESCRIPTION:"Count of cre alleles associated with esophagus",
+        SQLSTATEMENT:TEMPLATE_CRE_STRUCTURE_SQL%"esophagus"
 },
-{	ID:"creBronchusEpitheliumAlleleCount",
-	DESCRIPTION:"Count of cre alleles associated with bronchus epithelium",
-	SQLSTATEMENT:TEMPLATE_CRE_STRUCTURE_SQL%"bronchus epithelium"
+{       ID:"creBronchusEpitheliumAlleleCount",
+        DESCRIPTION:"Count of cre alleles associated with bronchus epithelium",
+        SQLSTATEMENT:TEMPLATE_CRE_STRUCTURE_SQL%"bronchus epithelium"
 },
-{	ID:"creBronchialEpitheliumAlleleCount",
-	DESCRIPTION:"Count of cre alleles associated with bronchial epithelium",
-	SQLSTATEMENT:TEMPLATE_CRE_STRUCTURE_SQL%"bronchial epithelium"
+{       ID:"creBronchialEpitheliumAlleleCount",
+        DESCRIPTION:"Count of cre alleles associated with bronchial epithelium",
+        SQLSTATEMENT:TEMPLATE_CRE_STRUCTURE_SQL%"bronchial epithelium"
 },
-{	ID:"creUterusAlleleCount",
-	DESCRIPTION:"Count of cre alleles associated with uterus",
-	SQLSTATEMENT:TEMPLATE_CRE_STRUCTURE_SQL%"uterus"
+{       ID:"creUterusAlleleCount",
+        DESCRIPTION:"Count of cre alleles associated with uterus",
+        SQLSTATEMENT:TEMPLATE_CRE_STRUCTURE_SQL%"uterus"
 },
 # copy above lines to make more tests
 ]
---
+++ testdata/c.bat
@@ -0,0 +1,28 @@
+call retab CreTestData.py
+call 2to3 CreTestData.py
+call toUnix CreTestData.py
+echo "done with CreTestData.py"
+call retab GXDLitTestData.py
+call 2to3 GXDLitTestData.py
+call toUnix GXDLitTestData.py
+echo "done with GXDLitTestData.py"
+call retab GXDTestData.py
+call 2to3 GXDTestData.py
+call toUnix GXDTestData.py
+echo "done with GXDTestData.py"
+call retab MarkerTestData.py
+call 2to3 MarkerTestData.py
+call toUnix MarkerTestData.py
+echo "done with MarkerTestData.py"
+call retab TemplateTestData.py
+call 2to3 TemplateTestData.py
+call toUnix TemplateTestData.py
+echo "done with TemplateTestData.py"
+call retab TestData.py
+call 2to3 TestData.py
+call toUnix TestData.py
+echo "done with TestData.py"
+call retab VocabTestData.py
+call 2to3 VocabTestData.py
+call toUnix VocabTestData.py
+echo "done with VocabTestData.py"
