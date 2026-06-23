c60c5e9c217ce4cc41532fccefb59eaca75ab43a
PeteFrost <pete.frost@jax.org>
2026-06-23 12:04:51 -0400

    Updating file retrieval to ignore certification errors

--- lib/python/MMHCdbData.py
+++ lib/python/MMHCdbData.py
@@ -5,11 +5,16 @@ import logger
 import urllib.request, urllib.error, urllib.parse
 import config
 import gc
+import ssl
 
 class MMHCdbDatabase:
         # Returns allele IDs that have tumor data in MMHCdb
         def getAlleleIDs (self):
-                with urllib.request.urlopen(config.MMHCDB_ALLELE_URL) as f:
-                    contents = f.read()
-                    alleleIDs = contents.decode('utf-8').strip().split()
-                return alleleIDs
+
+            # adding this will cause the request to ignore any cert errors
+            myssl_context = ssl._create_unverified_context()
+
+            with urllib.request.urlopen(config.MMHCDB_ALLELE_URL, context=myssl_context) as f:
+                contents = f.read()
+                alleleIDs = contents.decode('utf-8').strip().split()
+            return alleleIDs
